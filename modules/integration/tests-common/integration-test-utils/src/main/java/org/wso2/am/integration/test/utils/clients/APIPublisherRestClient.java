11037d2a01c95a80d9a6a8ef35b1d2436995d4a5
Dimuthu Lanerolle <dimuthud@wso2.com>
2015-03-09 18:44:43 +0530

    Adding new tests

--- modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/utils/clients/APIPublisherRestClient.java
+++ modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/utils/clients/APIPublisherRestClient.java
@@ -380,4 +380,22 @@ public class APIPublisherRestClient {
 			throw new Exception("API Subscription failed> " + response.getData());
 		}
 	}
+
+    public HttpResponse updateResourceOfAPI(String provider, String apiName, String version, String swaggerRes)
+            throws Exception {
+        checkAuthentication();
+        this.requestHeaders.put("Content-Type", "application/x-www-form-urlencoded");
+
+        HttpResponse response = HttpRequestUtil.doPost(new URL(backEndUrl +
+                "/publisher/site/blocks/item-design/ajax/add.jag")
+                , "action=manage" + "&provider=" + provider + "&name=" + apiName + "&version=" +
+                version +"&swagger="+swaggerRes
+                , requestHeaders);
+        if (response.getResponseCode() == 200) {
+            //VerificationUtil.checkErrors(response);
+            return response;
+        } else {
+            throw new Exception("API Resource update failed> " + response.getData());
+        }
+    }
 }
