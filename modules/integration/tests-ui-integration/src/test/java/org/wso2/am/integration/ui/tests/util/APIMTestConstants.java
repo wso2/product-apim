d73ed4e3691d740c063e1845995b1afa11f24438
Nisala <nisalaniroshana@gmail.com>
2015-04-02 10:37:13 +0530

    Remove 'Private' visibility when in single tenant mode

--- modules/integration/tests-ui-integration/src/test/java/org/wso2/am/integration/ui/tests/util/APIMTestConstants.java
+++ modules/integration/tests-ui-integration/src/test/java/org/wso2/am/integration/ui/tests/util/APIMTestConstants.java
@@ -49,6 +49,51 @@ public class APIMTestConstants {
     public static final String STORE_HOME_PAGE_URL_VERIFICATION = "store/";
     public static final String STORE_REST_CLIENT_URL_VERIFICATION = "/rest-client.jag?";
 
+    public static final String APIMANAGEMENTCONSOLE_LOGIN_USERNAME_ID="txtUserName";
+    public static final String APIMANAGEMENTCONSOLE_LOGIN_PASSWORD_ID="txtPassword";
+    public static final String APIMCONSOLE_ADDTENANT_DOMAIN_ID="domain";
+    public static final String APIMCONSOLE_ADDTENANT_FIRSTNAME_ID="admin-firstname";
+    public static final String APIMCONSOLE_ADDTENANT_LASTNAME_ID="admin-lastname";
+    public static final String APIMCONSOLE_ADDTENANT_USERNAME_ID="admin";
+    public static final String APIMCONSOLE_ADDTENANT_PASSW0RD_ID="admin-password";
+    public static final String APIMCONSOLE_ADDTENANT_PASSWORDREPEAT_ID="admin-password-repeat";
+    public static final String APIMCONSOLE_ADDTENANT_EMAIL_ID="admin-email";
+
+    public static final String APIMANAGEMENTCONSOLE_LOGIN_USERNAME="admin";
+    public static final String APIMANAGEMENTCONSOLE_LOGIN_PASSWORD="admin";
+    public static final String APIMCONSOLE_DOMAIN="testDomainVisibilitytest.com";
+    public static final String APIMCONSOLE_ADDTENANT_FIRSTNAME="testFirstNameVisibilitytest";
+    public static final String APIMCONSOLE_ADDTENANT_LASTNAME="testLastNameVisibilitytest";
+    public static final String APIMCONSOLE_ADDTENANT_USERNAME="testVisibility";
+    public static final String APIMCONSOLE_ADDTENANT_PASSW0RD="testVisibility";
+    public static final String APIMCONSOLE_ADDTENANT_PASSWORDREPEAT="testVisibility";
+    public static final String APIMCONSOLE_ADDTENANT_EMAIL="testEmail@testDomainVisibilitytest.com";
+
+    public static final String APIPUBLISHER_LOGIN_USERNAME_ID="username";
+    public static final String APIPUBLISHER_LOGIN_PASSWORD_ID="pass";
+    public static final String APIPUBLISHER_LOGIN_BUTTON_ID="loginButton";
+    public static final String APIPUBLISHER_ADD_LINKTEXT="Add";
+    public static final String APIPUBLISHER_ADD_APIVISIBILITY_ID="visibility";
+    public static final String APIPUBLISHER_USERMENU_ID="userMenu";
+    public static final String APIPUBLISHER_LOGOUT_BUTTONCSS="button.btn.btn-danger";
+    public static final String APIPUBLISHER_ADD_APINAME_ID="name";
+    public static final String APIPUBLISHER_ADD_APICONTEXT_ID="context";
+    public static final String APIPUBLISHER_ADD_APIVERSION_ID="version";
+    public static final String APIPUBLISHER_ADD_APIRESOURCEURL_ID="resource_url_pattern";
+    public static final String APIPUBLISHER_ADD_HTTPVERB_CSS="input.http_verb_select";
+    public static final String APIPUBLISHER_ADD_ADDRESOURCE_ID="add_resource";
+    public static final String APIPUBLISHER_ADD_IMPLEMENT_ID="go_to_implement";
+    public static final String APIPUBLISHER_LOGIN_USERNAME="admin";
+    public static final String APIPUBLISHER_LOGIN_PASSWORD="admin";
+
+    public static final String APIPUBLISHER_ADD_APIVISIBILITY="Public";
+    public static final String APIPUBLISHER_ADD_APINAME_SUPERTENANT="TestSubcriptionsWithOnlySuperTenant";
+    public static final String APIPUBLISHER_ADD_APICONTEXT_SUPERTENANT="TestSubcriptionsWithOnlySuperTenant";
+    public static final String APIPUBLISHER_ADD_APINAME_MultipleTENANT="TestSubcriptionsWithMultipleTenants";
+    public static final String APIPUBLISHER_ADD_APICONTEXT_MultipleTENANT="TestSubcriptionsWithOnlyMultipleTenant";
+    public static final String APIPUBLISHER_ADD_APIVERSION="1.0.0";
+    public static final String APIPUBLISHER_ADD_APIRESOURCEURL="resource_url";
+
     public static final long MAX_LOOP_WAIT_TIME_MILLISECONDS = 60000;
     public static final long WAIT_TIME_VISIBILITY_ELEMENT_SECONDS = 60;
 
