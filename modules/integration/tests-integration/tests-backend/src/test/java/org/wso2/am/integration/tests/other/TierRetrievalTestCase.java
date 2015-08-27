994072c301888151e00ab424957811ab526bf195
Nuwan Wimalasekara <nuwanw@Nuwans-MacBook-Pro.local>
2015-08-27 18:23:03 +0530

    fixing test cases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/TierRetrievalTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/TierRetrievalTestCase.java
@@ -125,6 +125,6 @@ public class TierRetrievalTestCase extends APIMIntegrationBaseTest {
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
-        super.cleanup();
+        super.cleanUp();
     }
 }
