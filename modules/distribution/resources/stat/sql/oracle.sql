b44314487f070514f4c2f6a609a289c6bc5e9d4b
rukshan <ruky68@live.com>
2015-12-03 17:11:59 +0530

    fixed https://wso2.org/jira/browse/APIMANAGER-4319

--- modules/distribution/resources/stat/sql/oracle.sql
+++ modules/distribution/resources/stat/sql/oracle.sql
@@ -1,5 +1,5 @@
 CREATE TABLE API_REQUEST_SUMMARY ( api VARCHAR2(100), api_version VARCHAR2(100), version VARCHAR2(100),
-	apiPublisher VARCHAR2(100),consumerKey VARCHAR2(100),userId VARCHAR2(100), context VARCHAR2(100),max_request_time NUMBER, total_request_count INT, hostName VARCHAR2(100), year SMALLINT, month SMALLINT, day SMALLINT, time VARCHAR2(30),PRIMARY KEY(api,api_version,apiPublisher,consumerKey,userId,context,hostName,time));
+	apiPublisher VARCHAR2(100),consumerKey VARCHAR2(100),userId VARCHAR2(100), context VARCHAR2(100),max_request_time NUMBER(30), total_request_count INT, hostName VARCHAR2(100), year SMALLINT, month SMALLINT, day SMALLINT, time VARCHAR2(30),PRIMARY KEY(api,api_version,apiPublisher,consumerKey,userId,context,hostName,time));
 
 CREATE TABLE API_VERSION_USAGE_SUMMARY ( api VARCHAR2(100), version VARCHAR2(100),apiPublisher VARCHAR2(100),context VARCHAR2(100),
 	total_request_count INT,hostName VARCHAR2(100), year SMALLINT, month SMALLINT, day SMALLINT, time VARCHAR2(30), PRIMARY KEY(api,version,apiPublisher,context,hostName,time));
