726de50c9cab034344e393e243c579095b068868
Chamin Dias <chamin892@gmail.com>
2015-12-10 10:30:01 +0530

    add test cases

---
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/restapi/testcases/TierTestCase.java
@@ -0,0 +1,73 @@
+/*
+*Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+
+package org.wso2.am.integration.tests.restapi.testcases;
+
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+import org.wso2.am.integration.test.utils.base.APIMIntegrationBaseTest;
+import org.wso2.am.integration.tests.restapi.utils.RESTAPITestUtil;
+import org.wso2.carbon.automation.engine.annotations.ExecutionEnvironment;
+import org.wso2.carbon.automation.engine.annotations.SetEnvironment;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
+
+import static org.testng.Assert.assertTrue;
+
+@SetEnvironment(executionEnvironments = {ExecutionEnvironment.STANDALONE})
+public class TierTestCase extends APIMIntegrationBaseTest {
+
+    @Factory(dataProvider = "userModeDataProvider")
+    public TierTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
+
+    @DataProvider
+    public static Object[][] userModeDataProvider() {
+        return new Object[][]{
+                new Object[]{TestUserMode.SUPER_TENANT_ADMIN}
+        };
+    }
+
+    @BeforeClass(alwaysRun = true)
+    public void setEnvironment() throws Exception {
+        super.init(userMode);
+    }
+
+    @Test(groups = {"wso2.am"}, description = "REST API Implementation test : Tier handling test case")
+    public void testTiers() {
+
+        //todo path?????
+
+
+
+
+
+        boolean testSuccessStatus = new RESTAPITestUtil().testRestAPI("/Applications/APIManagerGit/product-apim/modules/integration/tests-integration/tests-backend/src/test/resources/rest-api-test-data/TierTestCase.txt");
+        assertTrue(testSuccessStatus);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+        super.cleanUp();
+    }
+
+}
