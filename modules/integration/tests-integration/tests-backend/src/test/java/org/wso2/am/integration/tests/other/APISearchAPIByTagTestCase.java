92b93f03ccd6621afda1ed4ae99017f5c9744cf7
rukshan <ruky68@live.com>
2016-04-05 10:50:38 +0530

    fixed test method order

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APISearchAPIByTagTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APISearchAPIByTagTestCase.java
@@ -145,7 +145,7 @@ public class APISearchAPIByTagTestCase extends APIMIntegrationBaseTest {
         apiPublisher.deleteAPI(API_NAME_2, API_VERSION, user.getUserName());
     }
 
-    @Test(groups = { "wso2.am" }, description = "API search by TAG")
+    @Test(groups = { "wso2.am" }, description = "API search by TAG", dependsOnMethods = "testAPICreation")
     public void testAPISearchByTag() throws Exception {
         String searchTerm;
         HttpResponse response;
