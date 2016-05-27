2e3a879d7cee52f46ce77af2defcc1d5ddeb5617
bhathiya <bhathiya@wso2.com>
2016-05-27 18:18:41 +0530

    Fixed CORS testcases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/header/CORSHeadersTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/header/CORSHeadersTestCase.java
@@ -70,7 +70,6 @@ public class CORSHeadersTestCase extends APIManagerLifecycleBaseTest {
     private static final String ACCESS_CONTROL_ALLOW_ORIGIN_HEADER_VALUE = "*";
     private static final String ACCESS_CONTROL_ALLOW_METHODS_HEADER = "Access-Control-Allow-Methods";
     private static final String ACCESS_CONTROL_ALLOW_METHODS_HEADER_VALUE = "GET";
-    private static final String ACCESS_CONTROL_ALLOW_METHODS_HEADER_ALL_VALUES = "GET,PUT,POST,DELETE,PATCH,OPTIONS";
     private static final String ACCESS_CONTROL_ALLOW_HEADERS_HEADER = "Access-Control-Allow-Headers";
     private static final String ACCESS_CONTROL_ALLOW_HEADERS_HEADER_VALUE
             = "authorization,Access-Control-Allow-Origin,Content-Type";
@@ -151,7 +150,7 @@ public class CORSHeadersTestCase extends APIManagerLifecycleBaseTest {
 
         header = pickHeader(responseHeaders, ACCESS_CONTROL_ALLOW_METHODS_HEADER);
         assertNotNull(header, ACCESS_CONTROL_ALLOW_METHODS_HEADER + " header is not available in the response.");
-        assertEquals(header.getValue(), ACCESS_CONTROL_ALLOW_METHODS_HEADER_ALL_VALUES,
+        assertEquals(header.getValue(), ACCESS_CONTROL_ALLOW_METHODS_HEADER_VALUE,
                      ACCESS_CONTROL_ALLOW_METHODS_HEADER + " header value mismatch.");
 
         header = pickHeader(responseHeaders, ACCESS_CONTROL_ALLOW_HEADERS_HEADER);
