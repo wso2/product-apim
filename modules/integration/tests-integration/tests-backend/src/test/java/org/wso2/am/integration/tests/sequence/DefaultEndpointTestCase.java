6f4f5a4aa04783536c35d9322c629052595dafb4
bhathiya <bhathiya@wso2.com>
2016-06-16 00:20:47 +0530

    Fixed testcase which fail when disconnected from internet

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/sequence/DefaultEndpointTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/sequence/DefaultEndpointTestCase.java
@@ -43,11 +43,7 @@ public class DefaultEndpointTestCase extends APIManagerLifecycleBaseTest {
         super.init();
         String providerName = user.getUserName();
 
-
-
         apiCreationRequestBean = new APICreationRequestBean(API_NAME, API_CONTEXT, API_VERSION_1_0_0, providerName);
-
-
         apiCreationRequestBean.setTags(API_TAGS);
         apiCreationRequestBean.setDescription(API_DESCRIPTION);
         String publisherURLHttp = getPublisherURLHttp();
@@ -99,7 +95,7 @@ public class DefaultEndpointTestCase extends APIManagerLifecycleBaseTest {
 
         assertEquals(response.getStatusLine().getStatusCode(), HTTP_RESPONSE_CODE_OK, "Invocation fails for GET request");
 
-        assertEquals(response.getHeaders("Content-Type")[0].getValue(), "application/json; charset=utf-8");
+        assertEquals(response.getHeaders("Content-Type")[0].getValue(), "application/xml");
     }
 
 
