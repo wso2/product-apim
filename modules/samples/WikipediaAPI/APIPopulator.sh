cda933c09124c0eb1db438234f71352614caa98d
sanjeewa-malalgoda <sanjeewa.malalgoda@gmail.com>
2016-10-17 11:57:47 +0530

    adding new changes

--- modules/samples/WikipediaAPI/APIPopulator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-START=$1
-COUNT=$2
-SERVER=http://localhost:9763
-curl -X POST -c cookies $SERVER/publisher/site/blocks/user/login/ajax/login.jag -d 'action=login&username=admin&password=admin';
-
-curl -X POST -b cookies $SERVER/publisher/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=WikipediaAPI&visibility=public&version=1.0.0&description=If you want to monitor a MediaWiki installation, or create a bot to automatically maintain one, you can use the MediaWiki web service API. The web service API provides direct, high-level access to the data contained in MediaWiki databases&endpointType=nonsecured&http_checked=http&https_checked=https&wsdl=&tags=wikipedia,mediawiki&tier=Silver&thumbUrl=https://upload.wikimedia.org/wikipedia/en/b/bc/Wiki.png&context=/wikipedia&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&resourceMethodAuthType-0=Application&resourceMethodThrottlingTier-0=Unlimited&uriTemplate-0=/*" -d'endpoint_config={"production_endpoints":{"url":"http://en.wikipedia.org/w/api.php","config":null},"endpoint_type":"http"}';
-
-curl -X POST -b cookies $SERVER/publisher/site/blocks/life-cycles/ajax/life-cycles.jag -d "name=WikipediaAPI&version=1.0.0&provider=admin&status=PUBLISHED&publishToGateway=true&action=updateStatus";
--- modules/samples/YahooPlaceFinder/APIPopulator.bat
+++
@@ -1,8 +0,0 @@
-set SERVER=http://localhost:9763
-curl -X POST -c cookies %SERVER%/publisher/site/blocks/user/login/ajax/login.jag -d "action=login&username=provider1&password=provider1"
-curl -X POST -b cookies %SERVER%/publisher/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=PlaceFinder&version=1.0.0&description=Place Finder&endpoint=http://where.yahooapis.com/geocode&wsdl=&tags=finder,open,social&tier=Silver&thumbUrl=http://images.ientrymail.com/webpronews/article_pics/yahoo-placefinder.jpg&context=/placeFinder&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&resourceMethodAuthType-0=Application&uriTemplate-0=/*"
-curl -X POST -b cookies %SERVER%/publisher/site/blocks/life-cycles/ajax/life-cycles.jag -d "name=PlaceFinder&version=1.0.0&provider=provider1&status=PUBLISHED&publishToGateway=true&action=updateStatus"
-
-
-
-
