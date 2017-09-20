57e25f9227c6c835f306ff99ab21e45d16c44370
sabeena1217 <sabeena1217@gmail.com>
2017-09-20 11:32:49 +0530

    added a script

---
+++ gateway/bin/gatewayInit.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#if [ "$OFFLINE_GATEWAY_REPO" = " " ]; then
+#    bin/ballerina run service services.bsz org/wso2/carbon/apimgt/gateway;
+#else
+#    bin/ballerina run service microservices.bsz microgateway;
+#fi
+
+#GW_HOME="$PWD"
+
+
+export GW_HOME="$PWD";
+
+
+DIR="microgateway"
+
+if [ "$(ls -A $DIR)" ]; then
+#     echo "Take action $DIR is not Empty"
+     bin/ballerina run service microservices.bsz microgateway;
+else
+#    echo "$DIR is Empty"
+    bin/ballerina run service services.bsz org/wso2/carbon/apimgt/gateway;
+fi
