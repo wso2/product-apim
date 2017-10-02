aa0993594950eb81c372fd909fa20f9f629a5e95
sabeena1217 <sabeena1217@gmail.com>
2017-10-02 09:48:57 +0530

    final commit

--- gateway/bin/microgatewayinit.sh
+++ gateway/bin/microgatewayinit.sh
@@ -1,23 +1,4 @@
 #!/bin/bash
 
-#if [ "$OFFLINE_GATEWAY_REPO" = " " ]; then
-#    bin/ballerina run service services.bsz org/wso2/carbon/apimgt/gateway;
-#else
-#    bin/ballerina run service microservices.bsz microgateway;
-#fi
-
-#GW_HOME="$PWD"
-
-
 export GW_HOME="$PWD";
 bin/ballerina run service microgateway.bsz microgateway;
-
-#DIR="microgateway"
-
-#if [ "$(ls -A $DIR)" ]; then
-#     echo "Take action $DIR is not Empty"
-#     bin/ballerina run service microservices.bsz microgateway;
-#else
-#    echo "$DIR is Empty"
-#    bin/ballerina run service services.bsz org/wso2/carbon/apimgt/gateway;
-#fi
