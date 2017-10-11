8670a5d79efc5598b3845bdbb1441cafb3a436bc
Isuru Uyanage <isuruuy@gmail.com>
2017-10-11 13:12:27 +0530

    add certificate

--- integration-tests/src/test/java/org/wso2/carbon/apimgt/rest/integration/tests/api/publisher/TestUtils.java
+++ integration-tests/src/test/java/org/wso2/carbon/apimgt/rest/integration/tests/api/publisher/TestUtils.java
@@ -1,6 +1,24 @@
+/*
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied. See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
+ */
+
 package org.wso2.carbon.apimgt.rest.integration.tests.api.publisher;
 
-import org.testng.Assert;
 import org.wso2.carbon.apimgt.rest.integration.tests.publisher.ApiException;
 import org.wso2.carbon.apimgt.rest.integration.tests.publisher.api.APICollectionApi;
 import org.wso2.carbon.apimgt.rest.integration.tests.publisher.api.APIIndividualApi;
@@ -52,15 +70,14 @@ public class TestUtils {
         APIList response = api.apisGet(10, 0, null, null);
         String ifMatch = null;
         String ifUnmodifiedSince = null;
-        for(int i=0; i < response.getCount(); i++)
-        {
+        for (int i = 0; i < response.getCount(); i++) {
             String apiId = response.getList().get(i).getId();
             apiClient.apisApiIdDelete(apiId, ifMatch, ifUnmodifiedSince);
         }
     }
 
-    public String createEndPoint(String endPointName, String endPointConfig, String username, String password) throws ApiException
-    {
+    public String createEndPoint(String endPointName, String endPointConfig, String username, String password)
+            throws ApiException {
         String ifNoneMatch = null;
         String ifModifiedSince = null;
         long MaxTps = 1000;
@@ -81,8 +98,7 @@ public class TestUtils {
         return response.getId();
     }
 
-    public void deleteEndPoint(String endPointId) throws ApiException
-    {
+    public void deleteEndPoint(String endPointId) throws ApiException {
         String ifMatch = null;
         String ifUnmodifiedSince = null;
         endpointIndividualApi.endpointsEndpointIdDelete(endPointId, ifMatch, ifUnmodifiedSince);
