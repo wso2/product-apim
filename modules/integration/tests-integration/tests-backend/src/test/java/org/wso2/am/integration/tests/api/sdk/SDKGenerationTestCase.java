a8414a68a2be46094f42a20f96358afc45a42ea7
Pubudu Gunatilaka <pubudu538@gmail.com>
2017-10-13 14:04:35 +0530

    Adding sdk generation test case for private APIs

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/api/sdk/SDKGenerationTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/api/sdk/SDKGenerationTestCase.java
@@ -23,6 +23,7 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.lang.RandomStringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.http.Header;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -110,6 +111,49 @@ public class SDKGenerationTestCase extends APIMIntegrationBaseTest {
         Assert.assertTrue(isSDKGenerationSuccessfulInSameTenant && isSDKGenerationSuccessfulAcrossTenants);
     }
 
+    @Test(groups = {"wso2.am"}, description = "SDK Generation test case for private apis")
+    public void testSDKGenerationForPrivateAPIs() throws Exception {
+        //login to API publisher as first tenant admin to create the API
+        apiPublisher = new APIPublisherRestClient(publisherURLHttp);
+        apiPublisher.login(apiProvider, firstTenantAdminPassword);
+        //prepare API to create and publish
+        String apiName = "PrivateAPI";
+        String apiContext = "privateContext";
+        String url = "https://localhost:9443/test";
+        APIRequest apiRequest = new APIRequest(apiName, apiContext, new URL(url));
+        String tags = "sdkGen";
+        apiRequest.setTags(tags);
+        String description = "This is test API create by API manager integration test.";
+        apiRequest.setDescription(description);
+        apiRequest.setVersion(apiVersion);
+        apiRequest.setSandbox(url);
+        apiRequest.setResourceMethod("GET");
+        apiRequest.setProvider(apiProvider);
+        //set API visibility to private hence this API should be visible within domain
+        apiRequest.setVisibility("private");
+        apiPublisher.addAPI(apiRequest);
+        //update the lifecycle of the API to Published state, so that is it visible in store
+        APILifeCycleStateRequest updateRequest = new APILifeCycleStateRequest(apiName, apiProvider,
+                APILifeCycleState.PUBLISHED);
+        apiPublisher.changeAPILifeCycleStatus(updateRequest);
+        apiPublisher.logout();
+
+        //check for SDK generation in same tenant domain,
+        //(i.e - tenant admin of first tenant logs into the API store)
+        //we can use the apiProvider as the user name hence this is a login to the same tenant domain
+        String sdkLanguage = "java";
+        boolean isSDKGenerationSuccessfulInSameTenant = generateSDK(apiProvider, firstTenantAdminPassword, apiName,
+                apiVersion, apiProvider, sdkLanguage);
+
+        //check for SDK generation when API is private
+        //(i.e - tenant admin of second tenant should log into the API store)
+        boolean isSDKGenerationSuccessfulForPrivateAPIs = generateSDK
+                (secondTenantAdminUserName + "@" + secondTenantDomain, secondTenantAdminPassword, apiName, apiVersion,
+                        apiProvider, sdkLanguage);
+
+        Assert.assertTrue(isSDKGenerationSuccessfulInSameTenant);
+    }
+
     /**
      * This method generates SDK for a given API for a given programming language
      *
@@ -130,6 +174,14 @@ public class SDKGenerationTestCase extends APIMIntegrationBaseTest {
         //using org.apache.http.HttpResponse because we need to write the response to a file using a byte array
         org.apache.http.HttpResponse sdkGenerationResponse = apiStore.generateSDKUpdated(language, apiName,
                 apiVersion, apiProvider);
+
+        Header header = sdkGenerationResponse.getFirstHeader("Content-disposition");
+        if(header == null) {
+            log.error("SDK generation failed for API : " + apiName + " " + apiVersion + " User : " +
+                    tenantAwareUserName);
+            return false;
+        }
+
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         sdkGenerationResponse.getEntity().writeTo(byteArrayOutputStream);
         //save the generated SDK at the temp directory with a random file name
