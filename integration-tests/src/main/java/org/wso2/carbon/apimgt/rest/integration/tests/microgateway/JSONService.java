ec06e1b108f8b14c698f48cc0abbcfb9a24cd4d9
sabeena1217 <sabeena1217@gmail.com>
2017-10-22 07:07:19 +0530

    after java unit testing

---
+++ integration-tests/src/main/java/org/wso2/carbon/apimgt/rest/integration/tests/microgateway/JSONService.java
@@ -0,0 +1,30 @@
+package org.wso2.carbon.apimgt.rest.integration.tests.microgateway;
+
+//import org.wso2.carbon.apimgt.rest.integration.tests.microgateway.Track;
+
+public class JSONService {
+
+//    @GET
+//    @Path("/get")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Track getTrackInJSON() {
+//
+//        Track track = new Track();
+//        track.setTitle("Enter Sandman");
+//        track.setSinger("Metallica");
+//
+//        return track;
+//
+//    }
+//
+//
+//    @POST
+//    @Path("/post")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response createTrackInJSON(Track track) {
+//
+//        String result = "Track saved : " + track;
+//        return Response.status(201).entity(result).build();
+//
+//    }
+}
