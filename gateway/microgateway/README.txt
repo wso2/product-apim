e8bf78f732c8264e51dd9024d939bff31d9ccf14
sabeena1217 <sabeena1217@gmail.com>
2017-11-24 16:02:02 +0530

    done

--- gateway/microgateway/README.txt
+++ gateway/microgateway/README.txt
@@ -1,7 +1,6 @@
 WSO2 Offline Micro Gateway with API key
 
-This is WSO2 Offline Micro Gateway with API key, powered by Ballerina. 
-
+This is WSO2 Offline Micro Gateway with API key, powered by Ballerina 0.89. 
 
 Building from the source
 
@@ -15,48 +14,23 @@ If you want to build APIM Gateway from the source code:
 
 Starting the server
 
-1. Go to "product-apimgt/gateway/target/wso2apim-gateway-<version>-SNAPSHOT" directory
-2. Create "apiKeys.json" file in the microgateway folder where the data is passed in the following format,
-{  
-   "apis":[  
-      {  
-         "name":"",
-         "context":"",
-         "version":"",
-         "securityScheme":,
-	 "lifeCycleStatus":"",
-         "resources":[  
-            {  
-               "uriTemplate":"",
-               "httpVerb":"",
-               "authType":"",
-               "policy":"",
-               "scope":""
-            },
-            {  
-               "uriTemplate":"",
-               "httpVerb":"",
-               "authType":"",
-               "policy":"",
-               "scope":""
-            }
-         ],
-         "apps":[  
-            {  
-               "name":"",
-               "sandbox":"",
-               "production":""
-            }
-         ]
-      }
-   ]
-}
-3. Include all the API.bal files in the microgateway folder.
-NOTE : Make sure the package name in each API file is changed to "microgateway"
+1. Place bal file and swagger file of each API in wso2apim-gateway-<version>-SNAPSHOT/microgateway folder.
 
-4. Create "microConf.json" file in the microgateway folder where the data is passed in the following format,
-NOTE : If you wish to start the gateway without analytics and throttling, this step is not needed.
-But you need to comment the analytics and throttling in the gateway "deployment.yaml" file.  
+NOTE 1: If any API files are not given, the microgateway will not start, example files are given in `product-apimgt/gateway/microgateway`
+NOTE 2: Make sure the package name in each API bal file is changed to "microgateway"
+NOTE 3: Include the security component in the swagger structure, providing the api keys. (check for example files give in `product-apimgt/gateway/microgateway`)
+"security": [{
+		"api_key": [
+			"api_key_1",
+			"api_key_2",
+			"api_key_3",
+			"api_key_4"
+		]
+}]
+
+
+2. Place "microConf.json" file in the microgateway folder to pass the gateway configuration information,
+template is given below,
 
 {
 	"keyManagerInfo": {
@@ -94,7 +68,14 @@ But you need to comment the analytics and throttling in the gateway "deployment.
 		}
 	}
 }
-5. Execute the below command from the gateway home.
+
+Alternative:
+
+If you wish to start the gateway without analytics and throttling,you you need not to provide the "microConf.json" file, then you need to comment the analytics and throttling handlers in the gateway "deployment.yaml" file.
+
+Note: analytics and throttling are not enabled yet. For this pack "microConf.json" will come by default
+
+3. Execute the below command from the gateway home.
 "bin/microgatewayinit.sh"
 
 
--- integration-tests/src/main/java/org/wso2/carbon/apimgt/rest/integration/tests/microgateway/MockedApiClient.java
+++ integration-tests/src/main/java/org/wso2/carbon/apimgt/rest/integration/tests/microgateway/MockedApiClient.java
@@ -3,40 +3,28 @@ package org.wso2.carbon.apimgt.rest.integration.tests.microgateway;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.*;
-//import javax.net.ssl.SSLSocketFactory;
-//import java.util.ArrayList;
-//import java.util.List;
-//import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-//import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-//import javax.net.ssl.HostnameVerifier;
-//import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
-//import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
-
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.BasicClientConnectionManager;
-import org.apache.http.message.BasicNameValuePair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MockedApiClient {
+
+    Logger logger = LoggerFactory.getLogger(MockedApiClient.class);
 
     public static HttpClient createClient() {
         HttpClient client = null;
@@ -73,7 +61,6 @@ public class MockedApiClient {
         String output = null;
             HttpClient client = createClient();
             HttpGet request = new HttpGet("https://localhost:9092/"+context);
-          //  HttpGet request = new HttpGet("https://www.mocky.io/v2/59a96c49100000300d3e0afa")
         try {
             request.addHeader("apikey",apiKey);
             HttpResponse response = client.execute(request);
@@ -84,34 +71,9 @@ public class MockedApiClient {
             output = rd.readLine();
             System.out.println(output);
         } catch (IOException ioe) {
-
+            logger.info("Error occured", ioe);
         }
         return output;
     }
 
-    public void post() {
-//        HttpClient client = HttpClientBuilder.create().build();
-//        HttpPost post = new HttpPost("http://www.technicalkeeda.com/post-request");
-//        try {
-//            List <NameValuePair> nameValuePairs = new ArrayList <NameValuePair> ();
-//            nameValuePairs.add(new BasicNameValuePair("name", "Yashwant"));
-//            post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//
-//            HttpResponse response = client.execute(post);
-//
-//            int responseCode = response.getStatusLine().getStatusCode();
-//            System.out.println("**POST** request Url: " + post.getURI());
-//            System.out.println("Parameters : " + nameValuePairs);
-//            System.out.println("Response Code: " + responseCode);
-//            System.out.println("Content:-\n");
-//            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-//            String line = "";
-//            while ((line = rd.readLine()) != null) {
-//                System.out.println(line);
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-    }
 }
