d9c2405a91f78afe82ce324da8e64e1433f4bcfb
tharikaGitHub <tharika.m92@gmail.com>
2018-02-23 14:22:25 +0530

    Pack identity-anonymization-tool to APIM

---
+++ modules/distribution/product/src/main/bin/forgetme/forgetme.bat
@@ -0,0 +1,10 @@
+rem ----- Only set CARBON_HOME if not already set ----------------------------
+:checkServer
+setlocal enabledelayedexpansion
+rem %~sdp0 is expanded pathname of the current script under NT with spaces in the path removed
+if "%CARBON_HOME%"=="" set CARBON_HOME=%~sdp0..
+SET curDrive=%cd:~0,1%
+SET wsasDrive=%CARBON_HOME:~0,1%
+if not "%curDrive%" == "%wsasDrive%" %wsasDrive%:
+cd %CARBON_HOME%
+call %CARBON_HOME%\repository\components\tools\identity-anonymization-tool\bin\forget-me.bat
