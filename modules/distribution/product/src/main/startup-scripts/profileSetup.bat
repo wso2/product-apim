6ce8fe440db90a470c77b21e5ce82ac158f9ed8c
sarubi <sarubi>
2018-06-14 19:19:52 +0530

    Fixed issue in deletion of exploded webbapps files

--- modules/distribution/product/src/main/startup-scripts/profileSetup.bat
+++ modules/distribution/product/src/main/startup-scripts/profileSetup.bat
@@ -1,18 +1,18 @@
 @echo off
-# ----------------------------------------------------------------------------
-#  Copyright 2018 WSO2, Inc. http://www.wso2.org
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+rem ----------------------------------------------------------------------------
+rem  Copyright 2018 WSO2, Inc. http://www.wso2.org
+rem
+rem  Licensed under the Apache License, Version 2.0 (the "License");
+rem  you may not use this file except in compliance with the License.
+rem  You may obtain a copy of the License at
+rem
+rem      http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem  Unless required by applicable law or agreed to in writing, software
+rem  distributed under the License is distributed on an "AS IS" BASIS,
+rem  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem  See the License for the specific language governing permissions and
+rem  limitations under the License.
 
 set usertLocation=%cd%
 set pathToApiManagerXML=..\repository\conf\api-manager.xml
