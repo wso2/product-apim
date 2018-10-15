5d04db0dcb6da2317c4f5de5a8cc7341423e6075
Tharindu Wijewardane <tharinduw@wso2.com>
2018-10-15 11:08:20 +0530

    Upgrade OSGi version, use upgraded P2 plugin, C4, metrics and apimgt to support java 10
    upgrade OSGi version, use upgraded C4, metrics and apimgt versions
    
    modify wso2server.sh to support java 10

--- modules/distribution/product/src/main/startup-scripts/wso2server.sh
+++ modules/distribution/product/src/main/startup-scripts/wso2server.sh
@@ -222,10 +222,11 @@ elif [ "$CMD" = "version" ]; then
 fi
 
 # ---------- Handle the SSL Issue with proper JDK version --------------------
-jdk_17=`$JAVA_HOME/bin/java -version 2>&1 | grep "1.[7|8]"`
-if [ "$jdk_17" = "" ]; then
+java_version=$("$JAVACMD" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+java_version_formatted=$(echo "$java_version" | awk -F. '{printf("%02d%02d",$1,$2);}')
+if [ $java_version_formatted -lt 0107 ] || [ $java_version_formatted -gt 1000 ]; then
    echo " Starting WSO2 Carbon (in unsupported JDK)"
-   echo " [ERROR] CARBON is supported only on JDK 1.7 and 1.8"
+   echo " [ERROR] CARBON is supported only on JDK 1.7, 1.8, 9 and 10"
 fi
 
 CARBON_XBOOTCLASSPATH=""
@@ -252,13 +253,25 @@ for t in "$CARBON_HOME"/lib/commons-lang*.jar
 do
     CARBON_CLASSPATH="$CARBON_CLASSPATH":$t
 done
+
+if [ $java_version_formatted -ge 0900 ]; then
+    for f in "$CARBON_HOME"/lib/endorsed/*.jar
+    do
+       if [ "$f" != "$CARBON_HOME/lib/endorsed/*.jar" ];then
+           CARBON_CLASSPATH="$CARBON_CLASSPATH":$f
+       fi
+    done
+fi
+
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
   JAVA_HOME=`cygpath --absolute --windows "$JAVA_HOME"`
   CARBON_HOME=`cygpath --absolute --windows "$CARBON_HOME"`
   AXIS2_HOME=`cygpath --absolute --windows "$CARBON_HOME"`
   CLASSPATH=`cygpath --path --windows "$CLASSPATH"`
-  JAVA_ENDORSED_DIRS=`cygpath --path --windows "$JAVA_ENDORSED_DIRS"`
+  if [ $java_version_formatted -le 0108 ]; then
+    JAVA_ENDORSED_DIRS=`cygpath --path --windows "$JAVA_ENDORSED_DIRS"`
+  fi
   CARBON_CLASSPATH=`cygpath --path --windows "$CARBON_CLASSPATH"`
   CARBON_XBOOTCLASSPATH=`cygpath --path --windows "$CARBON_XBOOTCLASSPATH"`
 fi
@@ -290,6 +303,14 @@ echo "Using Java memory options: $JVM_MEM_OPTS"
 #To monitor a Carbon server in remote JMX mode on linux host machines, set the below system property.
 #   -Djava.rmi.server.hostname="your.IP.goes.here"
 
+JAVA_VER_BASED_OPTS=""
+if [ $java_version_formatted -le 0108 ]; then
+    JAVA_VER_BASED_OPTS="-Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS"
+fi
+if [ $java_version_formatted -ge 0900 ]; then
+    JAVA_VER_BASED_OPTS="--add-modules=java.activation,java.xml.bind"
+fi
+
 while [ "$status" = "$START_EXIT_STATUS" ]
 do
     $JAVACMD \
@@ -300,7 +321,7 @@ do
     $JAVA_OPTS \
     -Dcom.sun.management.jmxremote \
     -classpath "$CARBON_CLASSPATH" \
-    -Djava.endorsed.dirs="$JAVA_ENDORSED_DIRS" \
+    $JAVA_VER_BASED_OPTS \
     -Djava.io.tmpdir="$CARBON_HOME/tmp" \
     -Dcatalina.base="$CARBON_HOME/lib/tomcat" \
     -Dwso2.server.standalone=true \
