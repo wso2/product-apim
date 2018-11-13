a1530f18a221b7509e2b8eac4a0348e2432fa37e
achini <aaujayasena@gmail.com>
2018-11-13 11:34:59 +0530

    add new test method 1.1.1.5- testRESTAPICreationWithOptionalValuesupdate

--- product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api-and-apply-qos/1.1.1-create-rest-api-from-scratch/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationTestCase.java
+++ product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api-and-apply-qos/1.1.1-create-rest-api-from-scratch/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationTestCase.java
@@ -15,6 +15,8 @@
 */
 package org.wso2.am.scenario.tests.rest.api.creation;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -23,10 +25,12 @@ import org.wso2.am.scenario.test.common.APIPublisherRestClient;
 import org.wso2.am.scenario.test.common.APIRequest;
 import org.wso2.am.scenario.test.common.ScenarioTestBase;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
-
+import org.json.JSONObject;
+import org.testng.Assert;
 import java.util.Properties;
 
 public class RESTApiCreationTestCase extends ScenarioTestBase {
+    private static final Log log = LogFactory.getLog(APIRequest.class);
 
     private APIPublisherRestClient apiPublisher;
     private String publisherURLHttp;
@@ -38,6 +42,30 @@ public class RESTApiCreationTestCase extends ScenarioTestBase {
     private String apiVersion = "1.0.0";
     private String apiResource = "/find";
     private String apiVisibility = "public";
+
+    private String description = "This is a API creation description";
+    private String tag = "APICreationTag";
+    private String tierAvailability= "Silver";
+    private String tierCollection = "Silver";
+    private String apiTier="Silver";
+    private String bizOwner = "wso2Test";
+    private String bizOwnerMail = "wso2test@gmail.com";
+    private String techOwner = "wso2";
+    private String techOwnerMail = "wso2@gmail.com";
+    private String endpointType = "secured";
+    private String endpointAuthType = "basicAuth";
+    private String epUsername = "wso2";
+    private String epPassword = "wso2123";
+    private String default_version_checked = "default_version";
+    private String responseCache = "enabled";
+    private String cacheTimeout = "300";
+    private String subscriptions = "all_tenants";
+    private String http_checked = "http";
+    private String https_checked="";
+    private String inSequence = "debug_in_flow";
+    private String outSequence = "debug_out_flow";
+
+    private String backendEndPoint = "http://ws.cdyne.com/phoneverify/phoneverify.asmx";
 
     @BeforeClass(alwaysRun = true)
     public void init() throws APIManagerIntegrationTestException {
@@ -103,9 +131,46 @@ public class RESTApiCreationTestCase extends ScenarioTestBase {
 
     }
 
+        @Test(description = "1.1.1.5")
+    public void testRESTAPICreationWithOptionalValues() throws Exception {
+        apiName = "PhoneVerificationOptional";
+        apiContext = "/phoneverifyOptional";
+
+        apiRequest = new APIRequest(apiName, apiContext, apiVisibility, apiVersion, apiResource, description, tag, tierAvailability, tierCollection,apiTier, backendEndPoint, bizOwner, bizOwnerMail, techOwner, techOwnerMail, endpointType, endpointAuthType, epUsername, epPassword, default_version_checked, responseCache, cacheTimeout, subscriptions, http_checked, https_checked, inSequence, outSequence );
+
+        //Design API with name,context,version,visibility,apiResource and with optional values (description and tags)
+        HttpResponse serviceResponse = apiPublisher.addAPI(apiRequest);
+        verifyResponse(serviceResponse);
+        validateOptionalFiled();
+
+    }
+
     @AfterTest(alwaysRun = true)
     public void destroy() throws Exception {
         HttpResponse serviceResponse = apiPublisher.deleteAPI(apiName, apiVersion, "admin");
         verifyResponse(serviceResponse);
+    }
+
+    public void validateOptionalFiled () throws APIManagerIntegrationTestException {
+        HttpResponse getApi = apiPublisher.getAPI(apiName, "admin");
+        JSONObject response = new JSONObject(getApi.getData());
+        log.info("API Infor : " + getApi.getData());
+        String version = response.getJSONObject("api").get("name").toString();
+        Assert.assertEquals(response.getJSONObject("api").get("bizOwner").toString(), bizOwner, "Expected bizOwner value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("bizOwnerMail").toString(), bizOwnerMail, "Expected bizOwnerMail value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("techOwner").toString(), techOwner, "Expected techOwner value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("techOwnerMail").toString(), techOwnerMail, "Expected techOwnerMail value not match with the actual value");
+        Assert.assertEquals(response.getJSONObject("api").get("endpointTypeSecured").toString(), "true", "Expected endpointType value not match with the actual value");
+        Assert.assertEquals(response.getJSONObject("api").get("endpointAuthTypeDigest").toString(), "false", "Expected endpointAuthType value not match with the actual value");
+        Assert.assertEquals(response.getJSONObject("api").get("epUsername").toString(), epUsername, "Expected epUsername value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("epPassword").toString(), epPassword, "Expected epPassword value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("isDefaultVersion").toString(), "true", "Expected default_version_checked value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("responseCache").toString(), "Enabled", "Expected responseCache: value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("cacheTimeout").toString(), cacheTimeout, "Expected cacheTimeout value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("subscriptionAvailability").toString(), subscriptions, "Expected subscriptions value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("transport_http").toString(), "checked", "Expected http_checked value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("transport_https").toString(), "", "Expected https_checked value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("inSequence").toString(), inSequence, "Expected inSequence: value not match");
+        Assert.assertEquals(response.getJSONObject("api").get("outSequence").toString(), outSequence, "Expected outSequence value not match");
     }
 }
