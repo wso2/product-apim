051009733160fbb3349b152a411b77f534d55b2f
fazlan-nazeem <fazlann@wso2.com>
2019-01-01 10:14:41 +0530

    Add user management util methods

--- product-scenarios/scenarios-common/src/main/java/org/wso2/am/scenario/test/common/ScenarioTestBase.java
+++ product-scenarios/scenarios-common/src/main/java/org/wso2/am/scenario/test/common/ScenarioTestBase.java
@@ -17,32 +17,59 @@
  */
 package org.wso2.am.scenario.test.common;
 
+import org.apache.axis2.AxisFault;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.Assert;
+import org.wso2.am.admin.clients.user.RemoteUserStoreManagerServiceClient;
+import org.wso2.am.integration.test.utils.APIManagerIntegrationTestException;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
+import org.wso2.carbon.integration.common.admin.client.AuthenticatorClient;
+import org.wso2.carbon.integration.common.admin.client.TenantManagementServiceClient;
+import org.wso2.carbon.integration.common.admin.client.UserManagementClient;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationConstants;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.rmi.RemoteException;
 import java.util.Properties;
+
+import org.wso2.carbon.tenant.mgt.stub.TenantMgtAdminServiceExceptionException;
+import org.wso2.carbon.tenant.mgt.stub.beans.xsd.TenantInfoBean;
+import org.wso2.carbon.user.mgt.stub.UserAdminUserAdminException;
+import org.wso2.carbon.utils.multitenancy.MultitenantUtils;
 
 public class ScenarioTestBase {
 
     private static final String INPUTS_LOCATION = System.getenv("DATA_BUCKET_LOCATION");
     private static final String INFRASTRUCTURE_PROPERTIES = "deployment.properties";
     private static final Log log = LogFactory.getLog(ScenarioTestBase.class);
+    protected static String publisherURL;
+    private static Properties infraProperties;
     public static final String PUBLISHER_URL = "PublisherUrl";
     public static final String STORE_URL = "StoreUrl";
-    public static final String SERVICE_URL = "keyManagerUrl";
+    public static final String KEYAMANAGER_URL = "keyManagerUrl";
     String resourceLocation = System.getProperty("framework.resource.location");
+
+    public ScenarioTestBase() {
+        infraProperties = getDeploymentProperties();
+        publisherURL = infraProperties.getProperty(PUBLISHER_URL);
+        if (publisherURL == null) {
+            publisherURL = "https://localhost:9443/publisher";
+        }
+        setKeyStoreProperties();
+    }
 
     /**
      * This is a utility method to load the deployment details.
@@ -75,6 +102,53 @@ public class ScenarioTestBase {
         }
     }
 
+    public static void addTenantAndActivate(String domain, String adminUsername, String adminPassword)
+            throws APIManagementException {
+        TenantManagementServiceClient tenantManagementServiceClient = null;
+        try {
+            tenantManagementServiceClient = getTenantManagementServiceClient();
+            TenantInfoBean tenantInfoBean = tenantManagementServiceClient.getTenant(domain);
+            if (tenantInfoBean.getActive()) {
+                tenantManagementServiceClient.activateTenant(domain);
+            } else {
+                tenantManagementServiceClient.addTenant(domain, adminUsername, adminPassword, "demo");
+                tenantManagementServiceClient.activateTenant(domain);
+            }
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to add new tenant and activate " + domain, e);
+        }
+    }
+
+    public static TenantManagementServiceClient getTenantManagementServiceClient() throws APIManagementException {
+        String keyManagerURL = infraProperties.getProperty(KEYAMANAGER_URL);
+        if (StringUtils.isEmpty(keyManagerURL)) {
+            keyManagerURL = "https://localhost:9443/services/";
+        }
+        AuthenticatorClient authenticatorClient = null;
+        try {
+            authenticatorClient = new AuthenticatorClient(keyManagerURL);
+            URL url = new URL(keyManagerURL);
+            String sessionCookie = authenticatorClient.login("admin", "admin", url.getHost());
+
+            TenantManagementServiceClient tenantManagementServiceClient = new TenantManagementServiceClient(keyManagerURL,
+                    sessionCookie);
+            return tenantManagementServiceClient;
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to create new tenantManagementClient ", e);
+        }
+    }
+
+    public static UserManagementClient getRemoteUserManagerClient(String adminUsername, String adminPassword)
+            throws AxisFault {
+        String keyManagerURL = infraProperties.getProperty(KEYAMANAGER_URL);
+        if (StringUtils.isEmpty(keyManagerURL)) {
+            keyManagerURL = "https://localhost:9443/services/";
+        }
+        UserManagementClient userManagementClient = new UserManagementClient(keyManagerURL, adminUsername,
+                adminPassword);
+        return userManagementClient;
+    }
+
     public void setKeyStoreProperties() {
         System.setProperty("javax.net.ssl.trustStore", resourceLocation + "/keystore/wso2carbon.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "wso2carbon");
@@ -90,4 +164,64 @@ public class ScenarioTestBase {
         Assert.assertFalse(responseData.getBoolean(APIMIntegrationConstants.API_RESPONSE_ELEMENT_NAME_ERROR),
                 "Error message received " + httpResponse.getData());
     }
+
+
+    public static void createUserWithCreatorRole(String username, String password,
+            String adminUsername, String adminPassword) throws APIManagementException {
+        UserManagementClient userManagementClient = null;
+        try {
+            userManagementClient = getRemoteUserManagerClient(adminUsername, adminPassword);
+            userManagementClient.addUser(username, password, new String[] { ScenarioTestConstants.CREATOR_ROLE }, username);
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to create user with creator role " + username, e);
+        }
+    }
+
+    public void createUserWithPublisherRole(String username, String password, String adminUsername,
+            String adminPassword) throws APIManagementException {
+        UserManagementClient userManagementClient = null;
+        try {
+            userManagementClient = getRemoteUserManagerClient(adminUsername, adminPassword);
+            userManagementClient
+                    .addUser(username, password, new String[] { ScenarioTestConstants.PUBLISHER_ROLE }, username);
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to create user with publisher role " + username, e);
+        }
+
+    }
+
+    public void createUserWithSubscriberRole(String username, String password,
+            String adminUsername, String adminPassword)
+            throws RemoteException, UserAdminUserAdminException, APIManagementException {
+        UserManagementClient userManagementClient = null;
+        try {
+            userManagementClient = getRemoteUserManagerClient(adminUsername, adminPassword);
+            userManagementClient
+                    .addUser(username, password, new String[] { ScenarioTestConstants.SUBSCRIBER_ROLE }, username);
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to create user with subscriber role " + username, e);
+        }
+    }
+
+    public void createUser(String username, String password, String[] roleList,
+            String adminUsername, String adminPassword) throws APIManagementException {
+        UserManagementClient userManagementClient = null;
+        try {
+            userManagementClient = getRemoteUserManagerClient(adminUsername, adminPassword);
+            userManagementClient.addUser(username, password, roleList, username);
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to create user with the provided role list " + roleList, e);
+        }
+    }
+
+    public void deleteUser(String username, String adminUsername, String adminPassword) throws APIManagementException {
+        UserManagementClient userManagementClient;
+        try {
+            userManagementClient = getRemoteUserManagerClient(adminUsername, adminPassword);
+            userManagementClient.deleteUser(username);
+        } catch (Exception e) {
+            throw new APIManagementException("Unable to delete user :" + username, e);
+        }
+    }
+
 }
