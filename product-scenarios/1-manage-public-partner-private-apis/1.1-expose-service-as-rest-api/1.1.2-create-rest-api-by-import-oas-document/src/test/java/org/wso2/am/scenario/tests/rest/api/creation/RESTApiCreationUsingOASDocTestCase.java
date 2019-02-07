2e31af1107daf366d0baf89166f8cf410afa88b2
Ishara Cooray <isharacooray@WSO2s-MacBook-Pro-2.local>
2019-02-07 17:29:47 +0530

    test scenario 1.1.2.4 and refactoring

--- product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api/1.1.2-create-rest-api-by-import-oas-document/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationUsingOASDocTestCase.java
+++ product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api/1.1.2-create-rest-api-by-import-oas-document/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationUsingOASDocTestCase.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.wso2.am.scenario.tests.rest.api.creation;
 
 import org.apache.commons.logging.Log;
@@ -31,6 +31,7 @@ import org.wso2.am.scenario.test.common.APIRequest;
 import org.wso2.am.scenario.test.common.ScenarioDataProvider;
 import org.wso2.am.scenario.test.common.ScenarioTestBase;
 import org.wso2.am.scenario.test.common.ScenarioTestConstants;
+import org.wso2.am.scenario.test.common.ScenarioTestUtils;
 import org.wso2.am.scenario.test.common.httpserver.SimpleHTTPServer;
 import org.wso2.carbon.apimgt.api.APIManagementException;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
@@ -40,12 +41,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static org.apache.axis2.transport.http.HTTPConstants.USER_AGENT;
 
@@ -72,6 +77,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
 
     @BeforeClass(alwaysRun = true)
     public void init() throws Exception {
+
         createUsers();
         apiPublisher = new APIPublisherRestClient(publisherURL);
         apiPublisher.login(APICreator, pw);
@@ -79,6 +85,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
 
     @Test(description = "1.1.2.1")
     public void createApiWithValidOAS2DocumentAsJSONFile() throws Exception {
+
         swagger_file = new File(resourceLocation + File.separator + "swaggerFiles/OAS2Document.json");
 
         //Import api definition from swagger file
@@ -87,7 +94,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         Assert.assertTrue(designResponse1.getData().contains("imported"));
 
         //Save the API with imported values
-        String payload = readFromFile(swagger_file.getAbsolutePath());
+        String payload = ScenarioTestUtils.readFromFile(swagger_file.getAbsolutePath());
         JSONObject json = new JSONObject(payload);
         apiName = json.getJSONObject("info").get("title").toString();
         apiContext = json.get("basePath").toString();
@@ -126,7 +133,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         Assert.assertTrue(designResponse1.getData().contains("imported"));
 
         //Save the API with imported values
-        String payload = readFromFile(swagger_file.getAbsolutePath());
+        String payload = ScenarioTestUtils.readFromFile(swagger_file.getAbsolutePath());
         JSONObject json = new JSONObject(payload);
         String apiName = json.getJSONObject("info").get("title").toString();
         String apiContext = json.get("basePath").toString();
@@ -167,7 +174,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         Assert.assertTrue(designResponse1.getData().contains("imported"));
 
         //Save the API with imported values
-        String payload = readFromFile(swagger_file.getAbsolutePath());
+        String payload = ScenarioTestUtils.readFromFile(swagger_file.getAbsolutePath());
 
         Yaml yaml = new Yaml();
         Map<String, Object> map = (Map<String, Object>) yaml.load(payload);
@@ -209,7 +216,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         Assert.assertTrue(designResponse1.getData().contains("imported"));
 
         //Save the API with imported values
-        String payload = readFromFile(swagger_file.getAbsolutePath());
+        String payload = ScenarioTestUtils.readFromFile(swagger_file.getAbsolutePath());
 
         Yaml yaml = new Yaml();
         Map<String, Object> map = (Map<String, Object>) yaml.load(payload);
@@ -271,7 +278,6 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         }
     }
 
-    //TODO: Commented due to the SSL certification issue for read from url still occurs
     @Test(description = "1.1.2.2", dataProvider = "OASDocsWithJsonURL", dataProviderClass = ScenarioDataProvider.class)
     public void testCreateApiUsingValidOASDocumentFromJsonURL(String url) throws Exception {
 
@@ -284,7 +290,7 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
 
         new Thread(new SimpleHTTPServer()).start();
 
-        String payload = doGet(swagger_url);
+        String payload = ScenarioTestUtils.readFromURL(swagger_url);
         JSONObject json = new JSONObject(payload);
         String apiName = json.getJSONObject("info").get("title").toString();
         String context = json.get("basePath").toString();
@@ -304,55 +310,41 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
 
     }
 
-    //TODO: need to change to use SimpleHTTPServer
+    @Test(description = "1.1.2.4", dataProvider = "OASDocsWithYamlURL", dataProviderClass = ScenarioDataProvider.class)
+    public void testCreateApiUsingValidOASDocumentFromYamlURL(String url) throws Exception {
 
-//    @Test(description = "1.1.2.4", dataProvider = "OASDocsWithYamlURL", dataProviderClass = ScenarioDataProvider.class)
-//    public void testCreateApiUsingValidOASDocumentFromYamlURL(String url) throws Exception {
-//        swagger_url = url;
-//
-//        apiRequest = new APIRequest(import_definition_url, "", swagger_url, type);
-//
-//        HttpResponse serviceResponse = apiPublisher.designAPIWithOAS(apiRequest);
-//        Assert.assertTrue(serviceResponse.getData().contains("imported"));
-//
-//        String payload = readFromUrl(swagger_url);
-//
-//        Yaml yaml = new Yaml();
-//        Map<String, Object> map = (Map<String, Object>) yaml.load(payload);
-//        JSONObject json = new JSONObject(map);
-//        String apiName = json.getJSONObject("info").get("title").toString();
-//        String context = json.get("basePath").toString();
-//        String version = json.getJSONObject("info").get("version").toString();
-//        String payloadJson = json.toString();
-//
-//        apiRequest = new APIRequest(apiName, context, version);
-//        apiRequest.setSwagger(payloadJson);
-//
-//        serviceResponse = apiPublisher.designAPI(apiRequest);
-//        String name = (new JSONObject(serviceResponse.getData())).getJSONObject("data").get("apiName").toString();
-//        Assert.assertEquals(name, apiName);
-//
-//        serviceResponse = apiPublisher.deleteAPI(apiName, version, "admin");
-//        verifyResponse(serviceResponse);
-//    }
-//
-//    public static String readFromUrl(String url) throws IOException {
-//        URL myUrl = new URL(url);
-//        HttpsURLConnection conn = (HttpsURLConnection) myUrl.openConnection();
-//        InputStream is = conn.getInputStream();
-//        try {
-//            BufferedReader br = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-//            StringBuilder sb = new StringBuilder();
-//            int x;
-//            while ((x = br.read()) != -1) {
-//                sb.append((char) x);
-//            }
-//            String payloadText = sb.toString();
-//            return payloadText;
-//        } finally {
-//            is.close();
-//        }
-//    }
+        swagger_url = url;
+
+        apiRequest = new APIRequest(import_definition_url, "", swagger_url, type);
+
+        HttpResponse serviceResponse = apiPublisher.designAPIWithOAS(apiRequest);
+        Assert.assertTrue(serviceResponse.getData().contains("imported"));
+
+        new Thread(new SimpleHTTPServer()).start();
+
+        String payload = ScenarioTestUtils.readFromURL(swagger_url);
+
+        Yaml yaml = new Yaml();
+        Map<String, Object> map = (Map<String, Object>) yaml.load(payload);
+        JSONObject json = new JSONObject(map);
+        String apiName = json.getJSONObject("info").get("title").toString();
+        String context = json.get("basePath").toString();
+        String version = json.getJSONObject("info").get("version").toString();
+        String payloadJson = json.toString();
+
+        apiRequest = new APIRequest(apiName, context, version);
+        apiRequest.setSwagger(payloadJson);
+
+        serviceResponse = apiPublisher.designAPI(apiRequest);
+        String name = (new JSONObject(serviceResponse.getData())).getJSONObject("data").get("apiName").toString();
+        Assert.assertEquals(name, apiName);
+
+        serviceResponse = apiPublisher.deleteAPI(apiName, version, APICreator);
+        verifyResponse(serviceResponse);
+
+        Thread.sleep(1000); // To avoid connection failure in the next iteration.
+
+    }
 
     @AfterClass(alwaysRun = true)
     public void RemoveAPI() throws Exception {
@@ -363,60 +355,6 @@ public class RESTApiCreationUsingOASDocTestCase extends ScenarioTestBase {
         apiPublisher.deleteAPI("PetApiSample_OAS3_YAML", apiVersion, admin);
         deleteUser(APICreator, admin, adminPw);
 
-    }
-
-    public static String readFromFile(String file_name) throws IOException {
-
-        BufferedReader br = new BufferedReader(new FileReader(file_name));
-        StringBuilder sb = new StringBuilder();
-        int x;
-        while ((x = br.read()) != -1) {
-            sb.append((char) x);
-        }
-        String payloadText = sb.toString();
-        return payloadText;
-    }
-
-    public String doGet(String url) {
-
-        StringBuffer response = new StringBuffer();
-        URL obj = null;
-        try {
-            obj = new URL(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        BufferedReader in = null;
-        HttpURLConnection con = null;
-        try {
-            con = (HttpURLConnection) obj.openConnection();
-
-            con.setRequestMethod("GET");
-
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            int responseCode = 0;
-            responseCode = con.getResponseCode();
-            System.out.println("GET Response Code :: " + responseCode);
-            if (responseCode == HttpURLConnection.HTTP_OK) { // success
-                in = new BufferedReader(new InputStreamReader(
-                        con.getInputStream()));
-                String inputLine;
-
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-
-            }
-        } catch (IOException e) {
-            log.error("Error in reading url : " + url, e);
-        } finally {
-            if (con != null) {
-                con.disconnect();
-            }
-        }
-        return response.toString();
     }
 
     /*
