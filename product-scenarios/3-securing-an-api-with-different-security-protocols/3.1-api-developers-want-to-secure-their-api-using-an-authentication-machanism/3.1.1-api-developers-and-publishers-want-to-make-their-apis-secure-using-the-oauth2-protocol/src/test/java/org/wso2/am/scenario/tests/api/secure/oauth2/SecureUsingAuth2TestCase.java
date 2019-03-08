9d637cd36d489b32f2ae6066f41870be7b847a03
mushthaq33 <mushthaq33@gmail.com>
2019-03-08 11:04:53 +0530

    Possible fix for a test failure

--- product-scenarios/3-securing-an-api-with-different-security-protocols/3.1-api-developers-want-to-secure-their-api-using-an-authentication-machanism/3.1.1-api-developers-and-publishers-want-to-make-their-apis-secure-using-the-oauth2-protocol/src/test/java/org/wso2/am/scenario/tests/api/secure/oauth2/SecureUsingAuth2TestCase.java
+++ product-scenarios/3-securing-an-api-with-different-security-protocols/3.1-api-developers-want-to-secure-their-api-using-an-authentication-machanism/3.1.1-api-developers-and-publishers-want-to-make-their-apis-secure-using-the-oauth2-protocol/src/test/java/org/wso2/am/scenario/tests/api/secure/oauth2/SecureUsingAuth2TestCase.java
@@ -129,7 +129,7 @@ public class SecureUsingAuth2TestCase extends ScenarioTestBase {
                 "Response code mismatched when api invocation. \n API response : " + apiResponse.getData());
     }
 
-    @Test(description = "3.1.1.2")
+    @Test(description = "3.1.1.2", dependsOnMethods = "testOAuth2Authorization")
     public void testResourceSetSecurityTypeAsApplicationInvokeByClientCredentials() throws Exception {
         Map<String, String> requestHeaders = new HashMap<>();
         requestHeaders.put("Authorization", "Bearer " + accessToken);
@@ -142,7 +142,7 @@ public class SecureUsingAuth2TestCase extends ScenarioTestBase {
                 "Response code mismatched when api invocation. \n API response : " + apiResponse.getData());
     }
 
-    @Test(description = "3.1.1.3")
+    @Test(description = "3.1.1.3", dependsOnMethods = "testOAuth2Authorization")
     public void testResourceSetSecurityTypeAsApplicationUserInvokeByPasswordGrantType() throws Exception {
         String accessTokenWithPasswordGrantType = generateAccessTokenByPasswordGrantType();
         Map<String, String> requestHeaders = new HashMap();
@@ -172,7 +172,7 @@ public class SecureUsingAuth2TestCase extends ScenarioTestBase {
         changeCustomAuthorizationHeaderInAPI("");
     }
 
-    @Test(description = "3.1.1.7")
+    @Test(description = "3.1.1.7", dependsOnMethods = "testOAuth2Authorization")
     public void testResourceSetSecurityTypeAsNoneCanInvokedAPIWithoutTokenHeader() throws Exception {
         Map<String, String> requestHeaders;
         requestHeaders = new HashMap<>();
