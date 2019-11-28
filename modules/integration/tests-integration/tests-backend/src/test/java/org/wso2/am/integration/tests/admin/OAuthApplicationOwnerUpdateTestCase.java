8b257be331f7186e2afb2a3ece249e0c4659ee6f
Samitha Chathuranga <rmschathuranga@gmail.com>
2019-11-28 17:21:36 +0530

    Migrate OAuthApplicationOwnerUpdateTestCase

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/admin/OAuthApplicationOwnerUpdateTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/admin/OAuthApplicationOwnerUpdateTestCase.java
@@ -22,67 +22,80 @@ import org.apache.commons.logging.LogFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.am.integration.clients.store.api.v1.dto.ApplicationDTO;
+import org.wso2.am.integration.clients.store.api.v1.dto.ApplicationKeyGenerateRequestDTO;
+import org.wso2.am.integration.test.impl.RestAPIStoreImpl;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationBaseTest;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationConstants;
-import org.wso2.am.integration.test.utils.bean.APPKeyRequestGenerator;
-import org.wso2.am.integration.test.utils.clients.APIStoreRestClient;
 import org.wso2.am.integration.test.utils.clients.AdminDashboardRestClient;
 import org.wso2.carbon.automation.engine.annotations.ExecutionEnvironment;
 import org.wso2.carbon.automation.engine.annotations.SetEnvironment;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 import org.wso2.carbon.integration.common.admin.client.UserManagementClient;
 
+import java.util.ArrayList;
+
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
+import static org.wso2.am.integration.test.utils.base.APIMIntegrationConstants.SUPER_TENANT_DOMAIN;
 
 /**
  * This test case is used to test OAuth application owner update
  */
 @SetEnvironment(executionEnvironments = { ExecutionEnvironment.ALL })
 public class OAuthApplicationOwnerUpdateTestCase extends APIMIntegrationBaseTest {
+
     private final Log log = LogFactory.getLog(OAuthApplicationOwnerUpdateTestCase.class);
     private UserManagementClient userManagementClient1 = null;
     private static final String USER_JOHN = "john1";
     private static final String USER_JOHN_PWD = "john1@";
-    private static final String JOHN_APPLICATION = "johnApp";
+    private static final String JOHN_APP = "johnApp";
     private static final String USER_MARY = "mary1";
     private static final String USER_MARY_PWD = "mary1@";
-    private static final String MARY_APPLICATION = "maryApp";
-    private static final String ADMIN_TENANT = "admin";
-    private static final String ADMIN_TENANT_PWD = "admin1@";
-    private static final String USER1_TENANT = "user1";
-    private static final String USER1_TENANT_PWD = "user1@";
-    private static final String USER1_TENANT_APP= "user1App";
-    private static final String USER2_TENANT = "user2";
-    private static final String USER2_TENANT_PWD = "user2@";
-    private static final String USER2_TENANT_APP= "user2App";
-    private static final String USER3_TENANT = "user3";
-    private static final String USER3_TENANT_PWD = "user3@";
-    private static final String USER3_TENANT_APP= "user3App";
+    private static final String MARY_APP = "maryApp";
+    private static final String TENANT_ADMIN = "admin";
+    private static final String TENANT_ADMIN_PWD = "admin1@";
+    private static final String TENANT_USER1 = "user1";
+    private static final String TENANT_USER1_PWD = "user1@";
+    private static final String TENANT_USER1_APP = "user1App";
+    private static final String TENANT_USER2 = "user2";
+    private static final String TENANT_USER2_PWD = "user2@";
+    private static final String TENANT_USER2_APP = "user2App";
+    private static final String TENANT_USER3 = "user3";
+    private static final String TENANT_USER3_PWD = "user3@";
+    private static final String TENANT_USER3_APP = "user3App";
     private static final String TENANT_DOMAIN = "tenant.com";
-    private static final String ADMIN_TENANT_DOMAIN = ADMIN_TENANT + "@" + TENANT_DOMAIN;
-    private static final String USER1_TENANT_DOMAIN = USER1_TENANT + "@" + TENANT_DOMAIN;
-    private static final String USER2_TENANT_DOMAIN = USER2_TENANT + "@" + TENANT_DOMAIN;
-    private static final String USER3_TENANT_DOMAIN = USER3_TENANT + "@" + TENANT_DOMAIN;
-    private static final String USER4_TENANT_DOMAIN = "user4" + "@" + TENANT_DOMAIN;
-    private static final String TIER = "10PerMin";
-    private String[] subscriberRole = { "Internal/subscriber" };
+    private static final String TENANT_ADMIN_WITH_DOMAIN = TENANT_ADMIN + "@" + TENANT_DOMAIN;
+    private static final String TENANT_ADMIN_APP = "tenantAdminApp";
 
-    private String storeURLHttp;
-    private APIStoreRestClient apiStore;
-    private APIStoreRestClient apiStoreRestClient2;
+    private static final String TENANT_USER1_WITH_DOMAIN = TENANT_USER1 + "@" + TENANT_DOMAIN;
+    private static final String TENANT_USER2_WITH_DOMAIN = TENANT_USER2 + "@" + TENANT_DOMAIN;
+    private static final String TENANT_USER3_WITH_DOMAIN = TENANT_USER3 + "@" + TENANT_DOMAIN;
+    private static final String TENANT_USER4_WITH_DOMAIN = "user4" + "@" + TENANT_DOMAIN;
+    private String[] subscriberRole = { APIMIntegrationConstants.APIM_INTERNAL_ROLE.SUBSCRIBER };
+
+    private RestAPIStoreImpl restAPIStoreClient1;
+    private RestAPIStoreImpl restAPIStoreClient2;
+    private RestAPIStoreImpl restAPIStoreClient3;
+    private RestAPIStoreImpl restAPIStoreClient4;
+    private RestAPIStoreImpl restAPIStoreClient5;
+    private RestAPIStoreImpl restAPIStoreClient6;
+
     private  AdminDashboardRestClient adminDashboardRestClient;
     private JSONObject ownerJsonObject;
+    private String appIdOfJohnApp;
+    private String appIdOfMaryApp;
+    private String appIdOfTenantAdminApp;
+    private String appIdOfTenantUser1App;
+    private String appIdOfTenantUser2App;
+    private String appIdOfTenantUser3App;
 
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         super.init();
-        storeURLHttp = getStoreURLHttp();
-        apiStore = new APIStoreRestClient(storeURLHttp);
-        apiStore.login(user.getUserName(), user.getPassword());
-        apiStoreRestClient2 = new APIStoreRestClient(storeURLHttp);
         adminDashboardRestClient = new AdminDashboardRestClient(getPublisherURLHttps());
         userManagementClient = new UserManagementClient(keyManagerContext.getContextUrls().getBackEndUrl(),
                 createSession(keyManagerContext));
@@ -90,64 +103,83 @@ public class OAuthApplicationOwnerUpdateTestCase extends APIMIntegrationBaseTest
         userManagementClient.addUser(USER_JOHN, USER_JOHN_PWD, subscriberRole, USER_JOHN);
         userManagementClient.addUser(USER_MARY, USER_MARY_PWD, subscriberRole, USER_MARY);
         // add a tenant domain
-        tenantManagementServiceClient.addTenant(TENANT_DOMAIN, ADMIN_TENANT_PWD, ADMIN_TENANT, "demo");
+        tenantManagementServiceClient.addTenant(TENANT_DOMAIN, TENANT_ADMIN_PWD, TENANT_ADMIN, "demo");
         userManagementClient1 = new UserManagementClient(keyManagerContext.getContextUrls().getBackEndUrl(),
-                ADMIN_TENANT_DOMAIN, ADMIN_TENANT_PWD);
+                TENANT_ADMIN_WITH_DOMAIN, TENANT_ADMIN_PWD);
         // add users within the tenant domain
-        userManagementClient1.addUser(USER1_TENANT, USER1_TENANT_PWD, subscriberRole, USER1_TENANT);
-        userManagementClient1.addUser(USER2_TENANT, USER2_TENANT_PWD, subscriberRole, USER2_TENANT);
-        userManagementClient1.addUser(USER3_TENANT, USER3_TENANT_PWD, subscriberRole, USER3_TENANT);
-        // create application in the store using carbon super user credentials
-        apiStore.login(USER_JOHN, USER_JOHN_PWD);
-        HttpResponse addApplicationOfJohn = apiStore.addApplication(JOHN_APPLICATION,
-                APIMIntegrationConstants.APPLICATION_TIER.UNLIMITED, "", "");
-        APPKeyRequestGenerator generateKeysOfJohnApp = new APPKeyRequestGenerator(JOHN_APPLICATION);
-        String responseJohnApp = apiStore.generateApplicationKey(generateKeysOfJohnApp).getData();
-        apiStore.logout();
-        apiStore.login(USER_MARY, USER_MARY_PWD);
-        HttpResponse addApplicationOfMary = apiStore.addApplication(MARY_APPLICATION,
-                APIMIntegrationConstants.APPLICATION_TIER.UNLIMITED, "", "");
-        APPKeyRequestGenerator generateKeysOfMaryApp = new APPKeyRequestGenerator(MARY_APPLICATION);
-        String responseMaryApp = apiStore.generateApplicationKey(generateKeysOfMaryApp).getData();
-        apiStore.logout();
-        // create applications in the store using tenant admin credentials
-        apiStoreRestClient2.login(ADMIN_TENANT_DOMAIN, ADMIN_TENANT_PWD);
-        HttpResponse adminAppResponse = apiStoreRestClient2.addApplication(
-                "adminApp", TIER, "", "super-tenant-app");
-        apiStoreRestClient2.logout();
-        // create applications in the store using tenant user credentials
-        apiStoreRestClient2.login(USER1_TENANT_DOMAIN, USER1_TENANT_PWD);
-        HttpResponse User1AppResponse = apiStoreRestClient2.addApplication(
-                USER1_TENANT_APP, "10PerMin", "", "");
-        APPKeyRequestGenerator generateKeysOfTenantUser1App = new APPKeyRequestGenerator(USER1_TENANT_APP);
-        String responseTenantUser1= apiStoreRestClient2.generateApplicationKey(generateKeysOfTenantUser1App).getData();
-        apiStoreRestClient2.logout();
-        apiStoreRestClient2.login(USER2_TENANT_DOMAIN, USER2_TENANT_PWD);
-        HttpResponse User2AppResponse = apiStoreRestClient2.addApplication(
-                USER2_TENANT_APP, "10PerMin", "", "");
-        APPKeyRequestGenerator generateKeysOfTenantUser2App = new APPKeyRequestGenerator(USER2_TENANT_APP);
-        String responseTenantUser2 = apiStoreRestClient2.generateApplicationKey(generateKeysOfTenantUser2App).getData();
-        apiStoreRestClient2.logout();
-        apiStoreRestClient2.login(USER3_TENANT_DOMAIN, USER3_TENANT_PWD);
-        HttpResponse User3AppResponse = apiStoreRestClient2.addApplication(
-                USER3_TENANT_APP, "10PerMin", "", "");
-        APPKeyRequestGenerator generateKeysOfTenantUser3App = new APPKeyRequestGenerator(USER3_TENANT_APP);
-        String responseTenantUser3 = apiStoreRestClient2.generateApplicationKey(generateKeysOfTenantUser3App).getData();
-        apiStoreRestClient2.logout();
+        userManagementClient1.addUser(TENANT_USER1, TENANT_USER1_PWD, subscriberRole, TENANT_USER1);
+        userManagementClient1.addUser(TENANT_USER2, TENANT_USER2_PWD, subscriberRole, TENANT_USER2);
+        userManagementClient1.addUser(TENANT_USER3, TENANT_USER3_PWD, subscriberRole, TENANT_USER3);
 
+        ArrayList grantTypes = new ArrayList();
+        grantTypes.add(APIMIntegrationConstants.GRANT_TYPE.CLIENT_CREDENTIAL);
+
+        // create application in the store using super tenant user john1's credentials
+        restAPIStoreClient1 = new RestAPIStoreImpl(USER_JOHN, USER_JOHN_PWD, SUPER_TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfJohnDTO = restAPIStoreClient1.addApplication(JOHN_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.UNLIMITED, "", "App of user John");
+        appIdOfJohnApp = appOfJohnDTO.getApplicationId();
+        restAPIStoreClient1.generateKeys(appIdOfJohnApp,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
+
+        // create application in the store using super tenant user mary1's credentials
+        restAPIStoreClient2 = new RestAPIStoreImpl(USER_MARY, USER_MARY_PWD, SUPER_TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfMaryDTO = restAPIStoreClient2.addApplication(MARY_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.UNLIMITED, "", "App of user Mary");
+        appIdOfMaryApp = appOfMaryDTO.getApplicationId();
+        restAPIStoreClient2.generateKeys(appIdOfMaryApp,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
+
+        // create application in the store using tenant admin user's credentials
+        restAPIStoreClient3 = new RestAPIStoreImpl(TENANT_ADMIN_WITH_DOMAIN, TENANT_ADMIN_PWD, TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfTenantAdminDTO = restAPIStoreClient3.addApplication(TENANT_ADMIN_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.UNLIMITED, "", "App of tenant admin");
+        appIdOfTenantAdminApp = appOfTenantAdminDTO.getApplicationId();
+        restAPIStoreClient3.generateKeys(appIdOfTenantAdminApp,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
+
+        // create application in the store using tenant user user1's credentials
+        restAPIStoreClient4 = new RestAPIStoreImpl(TENANT_USER1_WITH_DOMAIN, TENANT_USER1_PWD, TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfTenantUser1DTO = restAPIStoreClient4.addApplication(TENANT_USER1_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.TEN_PER_MIN, "", "App of tenant user 1");
+        appIdOfTenantUser1App = appOfTenantUser1DTO.getApplicationId();
+        restAPIStoreClient4.generateKeys(appIdOfTenantUser1App,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
+
+        // create application in the store using tenant user user2's credentials
+        restAPIStoreClient5 = new RestAPIStoreImpl(TENANT_USER2_WITH_DOMAIN, TENANT_USER2_PWD, TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfTenantUser2DTO = restAPIStoreClient5.addApplication(TENANT_USER2_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.TEN_PER_MIN, "", "App of tenant user 2");
+        appIdOfTenantUser2App = appOfTenantUser2DTO.getApplicationId();
+        restAPIStoreClient5.generateKeys(appIdOfTenantUser2App,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
+
+        // create application in the store using tenant user user3's credentials
+        restAPIStoreClient6 = new RestAPIStoreImpl(TENANT_USER3_WITH_DOMAIN, TENANT_USER3_PWD, TENANT_DOMAIN, storeURLHttps);
+        ApplicationDTO appOfTenantUser3DTO = restAPIStoreClient6.addApplication(TENANT_USER3_APP,
+                APIMIntegrationConstants.APPLICATION_TIER.TEN_PER_MIN, "", "App of tenant user 3");
+        appIdOfTenantUser3App = appOfTenantUser3DTO.getApplicationId();
+        restAPIStoreClient6.generateKeys(appIdOfTenantUser3App,
+                APIMIntegrationConstants.DEFAULT_TOKEN_VALIDITY_TIME, "",
+                ApplicationKeyGenerateRequestDTO.KeyTypeEnum.PRODUCTION, null, grantTypes );
     }
 
     @Test(groups = {"wso2.am"}, description = "Check whether the new owner is a valid subscriber")
     public void checkSubscriberValidity() throws Exception {
-        adminDashboardRestClient.login(ADMIN_TENANT_DOMAIN, ADMIN_TENANT_PWD);
-        updateOwner(USER3_TENANT_APP,USER4_TENANT_DOMAIN,USER3_TENANT_DOMAIN);
-        assertTrue(ownerJsonObject.getBoolean("error"), USER4_TENANT_DOMAIN +" is not a subscriber");
+        adminDashboardRestClient.login(TENANT_ADMIN_WITH_DOMAIN, TENANT_ADMIN_PWD);
+        updateOwner(TENANT_USER3_APP, TENANT_USER4_WITH_DOMAIN, TENANT_USER3_WITH_DOMAIN);
+        assertTrue(ownerJsonObject.getBoolean("error"), TENANT_USER4_WITH_DOMAIN +" is not a subscriber");
     }
 
     @Test(groups = {"wso2.am"}, description = "Update application ownership to a user from another tenant domain")
     public void updateApplicationOwnerAcrossTenant() throws Exception {
-        adminDashboardRestClient.login(ADMIN_TENANT_DOMAIN, ADMIN_TENANT_PWD);
-        updateOwner(USER1_TENANT_APP,USER_MARY,USER1_TENANT_DOMAIN);
+        adminDashboardRestClient.login(TENANT_ADMIN_WITH_DOMAIN, TENANT_ADMIN_PWD);
+        updateOwner(TENANT_USER1_APP,USER_MARY, TENANT_USER1_WITH_DOMAIN);
         assertTrue(ownerJsonObject.getBoolean("error"), "Unable to update application owner to the user "
                 + USER_MARY + " as this user does not belong to this domain");
     }
@@ -164,14 +196,37 @@ public class OAuthApplicationOwnerUpdateTestCase extends APIMIntegrationBaseTest
     public void updateApplicationOwner() throws Exception {
         //Update application owner for carbon super
         adminDashboardRestClient.login(user.getUserName(), user.getPassword());
-        updateOwner(MARY_APPLICATION,USER_JOHN,USER_MARY);
+        updateOwner(MARY_APP,USER_JOHN,USER_MARY);
         assertFalse(ownerJsonObject.getBoolean("error"), "Successfully update owner of the application "
-                + MARY_APPLICATION + ".");
+                + MARY_APP + ".");
         //Update application Owner for tenant domain
-        adminDashboardRestClient.login(ADMIN_TENANT_DOMAIN, ADMIN_TENANT_PWD);
-        updateOwner(USER2_TENANT_APP,USER1_TENANT_DOMAIN,USER2_TENANT_DOMAIN);
+        adminDashboardRestClient.login(TENANT_ADMIN_WITH_DOMAIN, TENANT_ADMIN_PWD);
+        updateOwner(TENANT_USER2_APP, TENANT_USER1_WITH_DOMAIN, TENANT_USER2_WITH_DOMAIN);
         assertFalse(ownerJsonObject.getBoolean("error"), "Successfully update owner of the application "
-                + USER2_TENANT_APP + ".");
+                + TENANT_USER2_APP + ".");
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+
+        restAPIStoreClient1.deleteApplication(appIdOfJohnApp);
+        restAPIStoreClient2.deleteApplication(appIdOfMaryApp);
+        restAPIStoreClient3.deleteApplication(appIdOfTenantAdminApp);
+        restAPIStoreClient4.deleteApplication(appIdOfTenantUser1App);
+        restAPIStoreClient5.deleteApplication(appIdOfTenantUser2App);
+        restAPIStoreClient6.deleteApplication(appIdOfTenantUser3App);
+
+        if (userManagementClient != null) {
+            userManagementClient.deleteUser(USER_JOHN);
+            userManagementClient.deleteUser(USER_MARY);
+        }
+        if (userManagementClient1 != null) {
+            userManagementClient1.deleteUser(TENANT_USER1);
+            userManagementClient1.deleteUser(TENANT_USER2);
+            userManagementClient1.deleteUser(TENANT_USER3);
+        }
+        tenantManagementServiceClient.deleteTenant(TENANT_DOMAIN);
+        super.cleanUp();
     }
 
     /**
