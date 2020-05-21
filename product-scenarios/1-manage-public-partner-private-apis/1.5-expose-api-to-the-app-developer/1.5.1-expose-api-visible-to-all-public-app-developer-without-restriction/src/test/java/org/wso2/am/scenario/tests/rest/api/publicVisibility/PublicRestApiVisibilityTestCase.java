9b65af4870446cffcccdd67d8a88ef7eb6d9c3b6
Prasanna Dangalla <prasannadangalla@gmail.com>
2020-05-21 15:09:27 +0530

    Refactored PublicRestApiVisibilityTestCase.java.

--- product-scenarios/1-manage-public-partner-private-apis/1.5-expose-api-to-the-app-developer/1.5.1-expose-api-visible-to-all-public-app-developer-without-restriction/src/test/java/org/wso2/am/scenario/tests/rest/api/publicVisibility/PublicRestApiVisibilityTestCase.java
+++ product-scenarios/1-manage-public-partner-private-apis/1.5-expose-api-to-the-app-developer/1.5.1-expose-api-visible-to-all-public-app-developer-without-restriction/src/test/java/org/wso2/am/scenario/tests/rest/api/publicVisibility/PublicRestApiVisibilityTestCase.java
@@ -17,113 +17,165 @@ package org.wso2.am.scenario.tests.rest.api.publicVisibility;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
-import org.wso2.am.integration.test.utils.APIManagerIntegrationTestException;
-import org.wso2.am.scenario.test.common.APIPublisherRestClient;
-import org.wso2.am.scenario.test.common.APIStoreRestClient;
+import org.wso2.am.integration.clients.publisher.api.v1.dto.APIDTO;
+import org.wso2.am.integration.clients.store.api.ApiClient;
+import org.wso2.am.integration.test.utils.base.APIMIntegrationConstants;
+import org.wso2.am.integration.test.utils.bean.APICreationRequestBean;
+import org.wso2.am.integration.test.utils.bean.APILifeCycleAction;
+import org.wso2.am.integration.test.utils.bean.APIResourceBean;
 import org.wso2.am.scenario.test.common.ScenarioTestBase;
-import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
-import org.wso2.am.integration.test.utils.bean.APILifeCycleState;
-import org.wso2.am.integration.test.utils.bean.APILifeCycleStateRequest;
-import org.wso2.am.scenario.test.common.APIRequest;
+import org.wso2.am.scenario.test.common.ScenarioTestConstants;
+import org.wso2.carbon.automation.engine.context.TestUserMode;
 
 import java.net.URL;
-import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertNotNull;
 
 public class PublicRestApiVisibilityTestCase extends ScenarioTestBase {
 
-    private APIPublisherRestClient apiPublisher;
-    private APIRequest apiRequest;
     private String apiName = "PhoneVerification1";
     private String apiContext = "/verify";
-    private String apiResource = "/find";
-    private String admin = "admin";
-    private String apiVisibility = "public";
     private String apiVersion = "1.0.0";
     private String tierCollection = "Gold,Bronze";
     private String backendEndPoint = "http://ws.cdyne.com/phoneverify/phoneverify.asmx";
 
-    private String tenantDomain = "carbon.super";
     private static final Log log = LogFactory.getLog(PublicRestApiVisibilityTestCase.class);
-    private final String ADMIN_LOGIN_USERNAME = "admin";
-    private static final long WAIT_TIME = 3 * 1000;
-    private APIStoreRestClient apiStore;
+    private static final String ADMIN_USERNAME = "admin";
+    private static final String ADMIN_PW = "admin";
+    private static final String TENANT_ADMIN_USERNAME = "admin@wso2.com";
+    private static final String TENANT_ADMIN_PW = "admin";
+    private static final String API_CREATOR_PUBLISHER_USERNAME = "micheal";
+    private static final String API_CREATOR_PUBLISHER_PW = "Micheal#123";
+    private static final String API_SUBSCRIBER_USERNAME = "andrew";
+    private static final String API_SUBSCRIBER_PW = "Andrew#123";
+    private static String apiID;
+
+
+    @Factory(dataProvider = "userModeDataProvider")
+    public PublicRestApiVisibilityTestCase(TestUserMode userMode) {
+        this.userMode = userMode;
+    }
 
     @BeforeClass(alwaysRun = true)
-    public void init() throws APIManagerIntegrationTestException {
-        apiPublisher = new APIPublisherRestClient(publisherURL);
-        apiPublisher.login("admin", "admin");
-        apiStore = new APIStoreRestClient(storeURL);
-        apiStore.login("admin","admin");
+    public void setEnvironment() throws Exception {
+        if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
+            createUserWithPublisherAndCreatorRole(API_CREATOR_PUBLISHER_USERNAME, API_CREATOR_PUBLISHER_PW,
+                    ADMIN_USERNAME, ADMIN_PW);
+            createUserWithSubscriberRole(API_SUBSCRIBER_USERNAME, API_SUBSCRIBER_PW, ADMIN_USERNAME, ADMIN_PW);
+        }
+
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            // create user in wso2.com tenant
+            addTenantAndActivate(ScenarioTestConstants.TENANT_WSO2, ADMIN_USERNAME, ADMIN_PW);
+            if (isActivated(ScenarioTestConstants.TENANT_WSO2)) {
+//                Add and activate wso2.com tenant
+                createUserWithPublisherAndCreatorRole(API_CREATOR_PUBLISHER_USERNAME, API_CREATOR_PUBLISHER_PW,
+                        TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
+                createUserWithSubscriberRole(API_SUBSCRIBER_USERNAME, API_SUBSCRIBER_PW, TENANT_ADMIN_USERNAME,
+                        TENANT_ADMIN_PW);
+            }
+        }
+        super.init(userMode);
     }
 
     @Test(description = "1.5.1.1 and 1.5.1.2")
     public void testVisibilityOfPublicAPIsWithoutLogin() throws Exception {
 
-        apiName = "PhoneVerification1";
-        apiContext = "/verify";
-        apiRequest = new APIRequest(apiName, apiContext, apiVisibility, apiVersion, apiResource, tierCollection,
-                new URL(backendEndPoint));
+        APICreationRequestBean apiCreationRequestBean = new APICreationRequestBean(apiName, apiContext, apiVersion,
+                "admin", new URL(backendEndPoint));
+        apiCreationRequestBean.setTiersCollection(tierCollection);
+        APIDTO apiDto = restAPIPublisher.addAPI(apiCreationRequestBean);
+        apiID = apiDto.getId();
+        assertNotNull(apiDto.getId(), "API creation fails");
+        restAPIPublisher.changeAPILifeCycleStatus(apiID, APILifeCycleAction.PUBLISH.getAction(), null);
 
-        HttpResponse apiCreationResponse = apiPublisher.addAPI(apiRequest);
-        assertEquals(apiCreationResponse.getResponseCode(), Response.Status.OK.getStatusCode(),
-                "Response Code miss matched when creating the API");
-        verifyResponse(apiCreationResponse);
+        ApiClient existingAPIClient = restAPIStore.apIsApi.getApiClient();
+        // Setting an unAuthenticatedClient.
+        ApiClient unAuthenticatedApiClient = new ApiClient();
+        unAuthenticatedApiClient.setBasePath(existingAPIClient.getBasePath());
+        restAPIStore.apIsApi.setApiClient(unAuthenticatedApiClient);
 
-        HttpResponse apiResponsePublisher = apiPublisher.getAPI(apiName, ADMIN_LOGIN_USERNAME, apiVersion);
-        verifyResponse(apiResponsePublisher);
-        assertTrue(apiResponsePublisher.getData().contains(apiName), apiName + " is not visible in publisher");
-        verifyResponse(apiResponsePublisher);
-
-        APILifeCycleStateRequest updateLifeCycle =
-                new APILifeCycleStateRequest(apiName, ADMIN_LOGIN_USERNAME, APILifeCycleState.PUBLISHED);
-
-        HttpResponse apiPublishResponse = apiPublisher.changeAPILifeCycleStatus(updateLifeCycle);
-        verifyResponse(apiPublishResponse);
-        assertTrue(apiResponsePublisher.getData().contains(apiName), apiName + " is not visible in publisher");
-        verifyResponse(apiResponsePublisher);
-
-        // wait till API indexed in Store
-        isAPIVisibleInStore(apiName, apiStore);
+        // Check visibility in store for unAuthenticated user.
+        if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
+            org.wso2.am.integration.clients.store.api.v1.dto.APIDTO apiResponseStore = restAPIStore.getAPI(apiDto.getId());
+            Assert.assertEquals(apiResponseStore.getId(), apiDto.getId(), "Response object API id mismatch");
+        }
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            org.wso2.am.integration.clients.store.api.v1.dto.APIDTO apiResponseStore = restAPIStore.apIsApi.apisApiIdGet(apiID, "wso2.com", null);
+            Assert.assertEquals(apiResponseStore.getId(), apiDto.getId(), "Response object API id mismatch");
+        }
+        // Setting the authenticated Client again.
+        restAPIStore.apIsApi.setApiClient(existingAPIClient);
+        restAPIPublisher.deleteAPI(apiDto.getId());
     }
 
     @Test(description = "1.5.1.2")
     public void testVisibilityOfPublicAPIResourcesWithoutLogin() throws Exception {
-        //Create an API
-        apiName = "PhoneVerification2";
-        apiContext = "/findVerification";
-        apiRequest = new APIRequest(apiName, apiContext, apiVisibility, apiVersion, apiResource, tierCollection,
-                new URL(backendEndPoint));
+        String resourcePath = "path1";
+        APICreationRequestBean apiCreationRequestBean = new APICreationRequestBean(apiName, apiContext, apiVersion,
+                "admin", new URL(backendEndPoint));
+        apiCreationRequestBean.setTiersCollection(tierCollection);
+        ArrayList<APIResourceBean> resourceBeanArrayList = new ArrayList<>();
+        resourceBeanArrayList.add(new APIResourceBean(APIMIntegrationConstants.HTTP_VERB_GET,
+                APIMIntegrationConstants.RESOURCE_AUTH_TYPE_NONE, APIMIntegrationConstants.RESOURCE_TIER.UNLIMITED, resourcePath));
+        apiCreationRequestBean.setResourceBeanList(resourceBeanArrayList);
+        APIDTO apiDto = restAPIPublisher.addAPI(apiCreationRequestBean);
+        apiID = apiDto.getId();
+        assertNotNull(apiDto.getId(), "API creation fails");
+        restAPIPublisher.changeAPILifeCycleStatus(apiID, APILifeCycleAction.PUBLISH.getAction(), null);
 
-        HttpResponse apiCreationResponse = apiPublisher.addAPI(apiRequest);
-        assertEquals(apiCreationResponse.getResponseCode(), Response.Status.OK.getStatusCode(),
-                "Response Code miss matched when creating the API");
-        verifyResponse(apiCreationResponse);
+        ApiClient existingAPIClient = restAPIStore.apIsApi.getApiClient();
+        // Setting an unAuthenticatedClient.
+        ApiClient unAuthenticatedApiClient = new ApiClient();
+        unAuthenticatedApiClient.setBasePath(existingAPIClient.getBasePath());
+        restAPIStore.apIsApi.setApiClient(unAuthenticatedApiClient);
 
-        HttpResponse apiResponsePublisher = apiPublisher.getAPI(apiName, ADMIN_LOGIN_USERNAME, apiVersion);
-        verifyResponse(apiResponsePublisher);
-        assertTrue(apiResponsePublisher.getData().contains(apiName), apiName + " is not visible in publisher");
-        verifyResponse(apiResponsePublisher);
-
-        APILifeCycleStateRequest updateLifeCycle =
-                new APILifeCycleStateRequest(apiName, ADMIN_LOGIN_USERNAME, APILifeCycleState.PUBLISHED);
-
-        HttpResponse apiPublishResponse = apiPublisher.changeAPILifeCycleStatus(updateLifeCycle);
-        verifyResponse(apiPublishResponse);
-
-        // wait till API indexed in Store
-        isAPIVisibleInStore(apiName, apiStore);
+        // Check visibility in store for unAuthenticated user.
+        if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
+            org.wso2.am.integration.clients.store.api.v1.dto.APIDTO apiResponseStore = restAPIStore.getAPI(apiDto.getId());
+            Assert.assertEquals(apiResponseStore.getId(), apiDto.getId(), "Response object API id mismatch");
+            Assert.assertTrue(apiResponseStore.getApiDefinition().contains(resourcePath), "Response path mismatch");
+        }
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            org.wso2.am.integration.clients.store.api.v1.dto.APIDTO apiResponseStore = restAPIStore.apIsApi.apisApiIdGet(apiID, "wso2.com", null);
+            Assert.assertEquals(apiResponseStore.getId(), apiDto.getId(), "Response object API id mismatch");
+            Assert.assertTrue(apiResponseStore.getApiDefinition().contains(resourcePath), "Response path mismatch");
+        }
+        // Setting the authenticated Client again.
+        restAPIStore.apIsApi.setApiClient(existingAPIClient);
+        restAPIPublisher.deleteAPI(apiDto.getId());
     }
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
+        if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
+            deleteUser(API_CREATOR_PUBLISHER_USERNAME, ADMIN_USERNAME, ADMIN_PW);
+            deleteUser(API_SUBSCRIBER_USERNAME, ADMIN_USERNAME, ADMIN_PW);
+        }
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            deleteUser(API_CREATOR_PUBLISHER_USERNAME, TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
+            deleteUser(API_SUBSCRIBER_USERNAME, TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
+            deactivateAndDeleteTenant(ScenarioTestConstants.TENANT_WSO2);
+        }
+    }
 
-        apiPublisher.deleteAPI("PhoneVerification1", apiVersion, admin);
-        apiPublisher.deleteAPI("PhoneVerification2", apiVersion, admin);
+
+    @DataProvider
+    public static Object[][] userModeDataProvider() throws Exception {
+        setup();
+        // return the relevant parameters for each test run
+        // 1) Super tenant API creator
+        // 2) Tenant API creator
+        return new Object[][]{
+                new Object[]{TestUserMode.SUPER_TENANT_USER},
+                new Object[]{TestUserMode.TENANT_USER},
+        };
     }
 }
