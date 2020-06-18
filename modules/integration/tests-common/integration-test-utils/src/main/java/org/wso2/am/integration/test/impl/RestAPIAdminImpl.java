66d9b3456e03d65f0080906a1c295c0fa237f1d3
SahanHerath <sahand.herath@gmail.com>
2020-06-19 02:15:51 +0530

    Added tests for Approval Workflow Executor

--- modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/impl/RestAPIAdminImpl.java
+++ modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/impl/RestAPIAdminImpl.java
@@ -16,16 +16,19 @@
 
 package org.wso2.am.integration.test.impl;
 
+import com.google.gson.Gson;
+import org.apache.commons.lang.StringUtils;
 import org.wso2.am.integration.clients.admin.ApiClient;
 import org.wso2.am.integration.clients.admin.ApiException;
 import org.wso2.am.integration.clients.admin.ApiResponse;
 import org.wso2.am.integration.clients.admin.api.KeyManagerCollectionApi;
 import org.wso2.am.integration.clients.admin.api.KeyManagerIndividualApi;
 import org.wso2.am.integration.clients.admin.api.SettingsApi;
-import org.wso2.am.integration.clients.admin.api.dto.KeyManagerDTO;
-import org.wso2.am.integration.clients.admin.api.dto.KeyManagerListDTO;
-import org.wso2.am.integration.clients.admin.api.dto.SettingsDTO;
+import org.wso2.am.integration.clients.admin.api.WorkflowCollectionApi;
+import org.wso2.am.integration.clients.admin.api.WorkflowsIndividualApi;
+import org.wso2.am.integration.clients.admin.api.dto.*;
 import org.wso2.am.integration.test.ClientAuthenticator;
+import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
 /**
  * This util class performs the actions related to APIDTOobjects.
@@ -35,6 +38,8 @@ public class RestAPIAdminImpl {
     public ApiClient apiAdminClient = new ApiClient();
     private KeyManagerCollectionApi keyManagerCollectionApi = new KeyManagerCollectionApi();
     private KeyManagerIndividualApi keyManagerIndividualApi = new KeyManagerIndividualApi();
+    public WorkflowCollectionApi workflowCollectionApi = new WorkflowCollectionApi();
+    public WorkflowsIndividualApi workflowsIndividualApi = new WorkflowsIndividualApi();
     private SettingsApi settingsApi = new SettingsApi();
     public static final String appName = "Integration_Test_App_Admin";
     public static final String callBackURL = "test.com";
@@ -52,7 +57,7 @@ public class RestAPIAdminImpl {
         String tokenURL = adminURl + "oauth2/token";
         String dcrURL = adminURl + "client-registration/v0.16/register";
         String accessToken = ClientAuthenticator
-                .getAccessToken("openid apim:admin_operations",
+                .getAccessToken("openid apim:admin_operations apim:api_workflow_view apim:api_workflow_approve",
                         appName, callBackURL, tokenScope, appOwner, grantType, dcrURL, username, password, tenantDomain,
                         tokenURL);
 
@@ -62,6 +67,8 @@ public class RestAPIAdminImpl {
         keyManagerCollectionApi.setApiClient(apiAdminClient);
         keyManagerIndividualApi.setApiClient(apiAdminClient);
         settingsApi.setApiClient(apiAdminClient);
+        workflowCollectionApi.setApiClient(apiAdminClient);
+        workflowsIndividualApi.setApiClient(apiAdminClient);
         this.tenantDomain = tenantDomain;
     }
 
@@ -93,5 +100,52 @@ public class RestAPIAdminImpl {
     public SettingsDTO getSettings() throws ApiException {
 
         return settingsApi.settingsGet();
+    }
+
+    public HttpResponse getWorkflowByExternalWorkflowReference(String externalWorkflowRef) throws ApiException {
+        WorkflowInfoDTO workflowInfodto = null;
+        HttpResponse response = null;
+        Gson gson = new Gson();
+        try {
+            workflowInfodto = workflowsIndividualApi.workflowsExternalWorkflowRefGet(externalWorkflowRef, null);
+        } catch (ApiException e) {
+            return new HttpResponse(gson.toJson(e.getResponseBody()), e.getCode());
+        }
+        if (StringUtils.isNotEmpty(workflowInfodto.getReferenceId())) {
+            response = new HttpResponse(gson.toJson(workflowInfodto), 200);
+        }
+        return response;
+    }
+
+    public HttpResponse getWorkflows(String workflowType) throws ApiException {
+        WorkflowListDTO workflowListdto = null;
+        HttpResponse response = null;
+        Gson gson = new Gson();
+        try {
+            workflowListdto = workflowCollectionApi.workflowsGet(null, null, null, null, workflowType);
+            response = new HttpResponse(gson.toJson(workflowListdto), 200);
+        } catch (ApiException e) {
+            return new HttpResponse(gson.toJson(e.getResponseBody()), e.getCode());
+        }
+        return response;
+    }
+
+    public HttpResponse updateWorkflowStatus(String workflowReferenceId) throws ApiException {
+        WorkflowDTO workflowdto = null;
+        HttpResponse response = null;
+        Gson gson = new Gson();
+
+        WorkflowDTO body = new WorkflowDTO();
+        WorkflowDTO.StatusEnum status = WorkflowDTO.StatusEnum.valueOf(WorkflowDTO.StatusEnum.class, "APPROVED");
+        body.setStatus(status);
+        body.setDescription("Approve workflow request.");
+        //body.setAttributes();
+        try {
+            workflowdto = workflowsIndividualApi.workflowsUpdateWorkflowStatusPost(workflowReferenceId, body);
+            response = new HttpResponse(gson.toJson(workflowdto), 200);
+        } catch (ApiException e) {
+            return new HttpResponse(gson.toJson(e.getResponseBody()), e.getCode());
+        }
+        return response;
     }
 }
