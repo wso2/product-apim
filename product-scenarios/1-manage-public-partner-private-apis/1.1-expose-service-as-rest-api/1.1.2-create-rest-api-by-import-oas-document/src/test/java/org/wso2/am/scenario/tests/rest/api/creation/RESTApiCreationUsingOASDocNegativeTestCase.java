e685a351134c7dbf698ac31c1d12159e97f4e667
TheRasanjana <snironr@gmail.com>
2020-06-18 22:51:10 +0530

    Refactor RESTApiCreationUsingOASDocTestCase

--- product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api/1.1.2-create-rest-api-by-import-oas-document/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationUsingOASDocNegativeTestCase.java
+++ product-scenarios/1-manage-public-partner-private-apis/1.1-expose-service-as-rest-api/1.1.2-create-rest-api-by-import-oas-document/src/test/java/org/wso2/am/scenario/tests/rest/api/creation/RESTApiCreationUsingOASDocNegativeTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,30 +15,34 @@
  */
 package org.wso2.am.scenario.tests.rest.api.creation;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Factory;
 import org.testng.annotations.Test;
-import org.wso2.am.scenario.test.common.APIPublisherRestClient;
-import org.wso2.am.scenario.test.common.APIRequest;
-import org.wso2.am.scenario.test.common.ScenarioDataProvider;
+import org.wso2.am.integration.clients.publisher.api.ApiException;
+import org.wso2.am.integration.clients.publisher.api.v1.dto.APIDTO;
+import org.wso2.am.integration.clients.publisher.api.v1.dto.APIOperationsDTO;
+import org.wso2.am.integration.test.impl.RestAPIPublisherImpl;
 import org.wso2.am.scenario.test.common.ScenarioTestBase;
 import org.wso2.am.scenario.test.common.ScenarioTestConstants;
 import org.wso2.am.scenario.test.common.ScenarioTestUtils;
-import org.wso2.am.scenario.test.common.httpserver.SimpleHTTPServer;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
-import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
-/*
- * This class provides test cases for creating NEW REST API from scratch Negative test scenarios.
- * 1.1.2.7
- *
- * */
 public class RESTApiCreationUsingOASDocNegativeTestCase extends ScenarioTestBase {
+    private static final Log log = LogFactory.getLog(RESTApiCreationUsingOASDocNegativeTestCase.class);
 
     private static final String ADMIN_USERNAME = "admin";
     private static final String ADMIN_PW = "admin";
@@ -49,22 +53,21 @@ public class RESTApiCreationUsingOASDocNegativeTestCase extends ScenarioTestBase
     private static final String API_SUBSCRIBER_USERNAME = "andrew";
     private static final String API_SUBSCRIBER_PW = "Andrew#123";
 
-    private APIPublisherRestClient apiPublisher;
-    private APIRequest apiRequest;
     private File swaggerFile;
-    private String importDefinitionUrl = "swagger-url";
-    private String importDefinitionFile = "swagger-file";
+    private String swaggerUrl;
     private String type = "rest";
     private String apiName;
     private String apiVersion;
     private String apiContext;
-    private String swaggerUrl;
-    private String APICreator = "APICreator";
-    private String pw = "wso2123$";
-    private final String admin = "admin";
-    private final String adminPw = "admin";
+    private String link;
+    private String apiId;
+    private String apiProviderName;
+    private List<String> apiIdList = new ArrayList<>();
+    private String apiProductionEndPointUrl;
+    private  String apiProductionEndpointPostfixUrl = "jaxrs_basic/services/customers/" + "customerservice/customers/123";
 
-    private String providerName;
+    private final static String OAS_V2 = "v2";
+    private final static String OAS_V3 = "v3";
 
     String resourceLocation = System.getProperty("test.resource.location");
 
@@ -77,75 +80,150 @@ public class RESTApiCreationUsingOASDocNegativeTestCase extends ScenarioTestBase
     public void setEnvironment() throws Exception {
         if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
             createUserWithPublisherAndCreatorRole(API_CREATOR_PUBLISHER_USERNAME, API_CREATOR_PUBLISHER_PW,
-                    ADMIN_USERNAME, ADMIN_PW);
+                                                  ADMIN_USERNAME, ADMIN_PW);
             createUserWithSubscriberRole(API_SUBSCRIBER_USERNAME, API_SUBSCRIBER_PW, ADMIN_USERNAME, ADMIN_PW);
         }
 
         if (this.userMode.equals(TestUserMode.TENANT_USER)) {
-            // create user in wso2.com tenant
+//           create user in wso2.com tenant
             addTenantAndActivate(ScenarioTestConstants.TENANT_WSO2, ADMIN_USERNAME, ADMIN_PW);
             if (isActivated(ScenarioTestConstants.TENANT_WSO2)) {
-                //Add and activate wso2.com tenant
+//           Add and activate wso2.com tenant
                 createUserWithPublisherAndCreatorRole(API_CREATOR_PUBLISHER_USERNAME, API_CREATOR_PUBLISHER_PW,
-                        TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
+                                                      TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
                 createUserWithSubscriberRole(API_SUBSCRIBER_USERNAME, API_SUBSCRIBER_PW, TENANT_ADMIN_USERNAME,
-                        TENANT_ADMIN_PW);
+                                             TENANT_ADMIN_PW);
             }
         }
-
-        setup();
         super.init(userMode);
-
     }
 
-//    @Test(description = "1.1.2.7") //todo fix uploaded swagger file not reading issue
-//    public void createApiWithInvalidOAS3DocumentAsJSONFile() throws Exception {
-//
-//        swaggerFile = new File(resourceLocation + File.separator + "swaggerFiles/InvalidOAS3Document.json");
-//
-//        //Import api definition from swagger file
-//        apiRequest = new APIRequest(importDefinitionFile, swaggerFile.getAbsolutePath(), "", type);
-//        String boundary = "===" + System.currentTimeMillis() + "===";
-////        apiPublisher.sertContentType("multipart/form-data; boundary=----WebKitFormBoundary3KQfws99yVtxs0Ow");
-//        HttpResponse designResponse1 = apiPublisher.designAPIWithOAS(apiRequest);
-//        verifyNegativeResponse(designResponse1);
-//
-//    }
 
-    @Test(description = "1.1.2.8", dataProvider = "InvaidOASDocs", dataProviderClass = ScenarioDataProvider.class)
-    public void testCreateApiUsingInvalidOASDocumentFromJsonURL(String url) throws Exception {
+    @Test(description = "1.1.2.7", enabled = false)
+    public void createApiWithInvalidOAS3DocumentAsJSONFile() throws Exception {
 
-        swaggerUrl = url;
+        swaggerFile = new File(resourceLocation + File.separator + "swaggerFiles/InvalidOAS3Document.json");
 
-        apiRequest = new APIRequest(importDefinitionUrl, "", swaggerUrl, type);
-        SimpleHTTPServer server = new SimpleHTTPServer();
-        new Thread(server).start();
-        HttpResponse serviceResponse = apiPublisher.designAPIWithOASURL(apiRequest);
-        server.stop();
-        Assert.assertTrue(serviceResponse.getData().contains("imported"), "Error importing swagger from : " + url);
+        APIDTO apiDTO = new APIDTO();
+        List<APIOperationsDTO> apiOperationsDTOs = new ArrayList<>();
+        List<String> tagList = new ArrayList<>();
 
-        //Save the API with imported values
-        new Thread( new SimpleHTTPServer()).start();
-        String payload = ScenarioTestUtils.readFromURL(swaggerUrl);
-        JSONObject json = new JSONObject(payload);
-        int paths = json.getJSONObject("paths").length();
-        //UI prevents creating the API when there are no resources added from the swagger or manually.
-        Assert.assertTrue(paths == 0, "Swagger doc contains resource paths.");
+        String payload = ScenarioTestUtils.readFromFile(swaggerFile.getAbsolutePath());
+        JSONObject jsonPayload = new JSONObject(payload);
+        apiName = "Swagger3SampleAPI2";
+        apiContext = "basePath2";
+        apiVersion = "1.0.0";
+        JSONObject paths = jsonPayload.getJSONObject("paths");
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            apiName=apiName+TestUserMode.TENANT_USER;
+            apiContext=apiContext+TestUserMode.TENANT_USER;}
+        apiDTO.setName(apiName);
+        apiDTO.setContext(apiContext);
+        apiDTO.setVersion(apiVersion);
+        apiDTO.setProvider(apiProviderName);
+
+        APIDTO responseAPIDTO = restAPIPublisher.addAPI(apiDTO, OAS_V3);
+        apiId = responseAPIDTO.getId();
+        apiIdList.add(apiId);
+        Assert.assertEquals(responseAPIDTO.getLifeCycleStatus(), "CREATED");
+        boolean updatedSwaggerSuccessfully = true;
+    try {
+        String swaggerResponse = restAPIPublisher.updateSwagger(apiId, payload);
+    } catch (ApiException e) {
+        Assert.assertTrue(e.getResponseBody().contains("Error while parsing OpenAPI definition"),"Invalid OAS Document was updated successfully!");
+        }
     }
 
-    @Test(description = "1.1.2.10", dataProvider = "OASDocsWithInvalidURL", dataProviderClass = ScenarioDataProvider.class)
-    public void createApiWithInvalidOAS3DocumentURL(String url) throws Exception {
+    @Test(description = "1.1.2.8", enabled = false)
+    public void testCreateApiUsingInvalidOASDocumentFromJsonURL() throws Exception {
+        String swaggerFileName = "InvalidOAS2Document.json";
 
-        swaggerUrl = url;
-        apiRequest = new APIRequest(importDefinitionUrl, "", swaggerUrl, type);
+        uploadFile(swaggerFileName);
+        APIDTO apiDTO = new APIDTO();
+        List<APIOperationsDTO> apiOperationsDTOs = new ArrayList<>();
+        List<String> tagList = new ArrayList<>();
+        String swaggerVersion;
+        JSONObject json;
+        String payload = ScenarioTestUtils.readFromURL(link);
+        json = new JSONObject(payload);
 
-        HttpResponse serviceResponse = apiPublisher.designAPIWithOASURL(apiRequest);
-        verifyNegativeResponse(serviceResponse);
+        if (json.get("swagger") != null) {
+            swaggerVersion = OAS_V2;
+            apiContext = json.get("basePath").toString();
+        } else {
+            swaggerVersion = OAS_V3;
+            apiContext = json.get("x-wso2-basePath").toString();
+        }
+        apiName = json.getJSONObject("info").get("title").toString();
+        apiVersion = json.getJSONObject("info").get("version").toString();
 
+        if (this.userMode.equals(TestUserMode.TENANT_USER)) {
+            apiName=apiName+TestUserMode.TENANT_USER;
+            apiContext=apiContext+TestUserMode.TENANT_USER;}
+
+        apiDTO.setName(apiName);
+        apiDTO.setContext(apiContext);
+        apiDTO.setVersion(apiVersion);
+
+        APIDTO responseAPIDTO = restAPIPublisher.addAPI(apiDTO, swaggerVersion);
+        apiId = responseAPIDTO.getId();
+        apiIdList.add(apiId);
+        Assert.assertEquals(responseAPIDTO.getLifeCycleStatus(), "CREATED");
+
+        try {
+          String swaggerResponse = restAPIPublisher.updateSwagger(apiId, payload);
+             }
+        catch (ApiException e) {
+        Assert.assertTrue(e.getResponseBody().contains("No resources found"),"Invalid OAS Document was updated successfully!");
+        }
+    }
+
+    private String uploadFile(String uploadfile){
+        String file = resourceLocation+ File.separator +"swaggerFiles/"+uploadfile;
+        String[] command = {"curl", "-k", "-v", "-F","file=@"+file ,"https://file.io"};
+
+        ProcessBuilder builder = new ProcessBuilder(command);
+        builder.redirectErrorStream(true);
+
+        String curlResult = null;
+        String line = "";
+    while (curlResult!=null) {
+    try {
+        Process process = builder.start();
+        BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        while (true) {
+            line = r.readLine();
+            if (line == null) {
+                break;
+            }
+            curlResult = curlResult + line;
+        }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}
+    String filename = StringUtils.substringBetween(curlResult, "link\":\"https://file.io/", "\",\"expiry\"");
+    Assert.assertNotNull(filename , "File not uploaded successfully!");
+    link = "https://file.io/"+filename;
+    return link ;
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void destroyAPIs() throws Exception {
+        RestAPIPublisherImpl restAPIPublisherNew;
+        restAPIPublisherNew = new RestAPIPublisherImpl(ADMIN_USERNAME, ADMIN_PW, publisherContext.getContextTenant().getDomain(), publisherURLHttps);
+        try {
+            for (String apiId : apiIdList) {
+                restAPIPublisherNew.deleteAPI(apiId);
+            }
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
     }
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
+//        clean the data
         if (this.userMode.equals(TestUserMode.SUPER_TENANT_USER)) {
             deleteUser(API_CREATOR_PUBLISHER_USERNAME, ADMIN_USERNAME, ADMIN_PW);
             deleteUser(API_SUBSCRIBER_USERNAME, ADMIN_USERNAME, ADMIN_PW);
@@ -155,6 +233,20 @@ public class RESTApiCreationUsingOASDocNegativeTestCase extends ScenarioTestBase
             deleteUser(API_SUBSCRIBER_USERNAME, TENANT_ADMIN_USERNAME, TENANT_ADMIN_PW);
             deactivateAndDeleteTenant(ScenarioTestConstants.TENANT_WSO2);
         }
+
+    }
+
+    @DataProvider
+    public static Object[][] userModeDataProvider() throws Exception {
+        setup();
+        // return the relevant parameters for each test run
+        // 1) Super tenant API creator
+        // 2) Tenant API creator
+        return new Object[][]{
+            new Object[]{TestUserMode.SUPER_TENANT_USER},
+//            new Object[]{TestUserMode.TENANT_USER},
+            };
     }
 
 }
+
