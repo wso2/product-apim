ce0895ac1c237afbc000912b8a141c3edcc30ef9
Prasanna Dangalla <prasannadangalla@gmail.com>
2020-07-21 11:58:57 +0530

    Formatted tests.

--- product-scenarios/2-collaborative-api-development/2.1-manage-publisher-portal-operations-for-users/2.1.1-manage-api-publishing-according-to-the-publisher-roles-and-permission/src/test/java/org/wso2/am/scenario/tests/rest/api/publisherRoleAndPermission/PublishAPIByValidRolePermissionCategoryTestCase.java
+++ product-scenarios/2-collaborative-api-development/2.1-manage-publisher-portal-operations-for-users/2.1.1-manage-api-publishing-according-to-the-publisher-roles-and-permission/src/test/java/org/wso2/am/scenario/tests/rest/api/publisherRoleAndPermission/PublishAPIByValidRolePermissionCategoryTestCase.java
@@ -306,9 +306,9 @@ public class PublishAPIByValidRolePermissionCategoryTestCase extends ScenarioTes
         // 1) Super tenant API creator
         // 2) Tenant API creator
         return new Object[][]{
-            new Object[]{TestUserMode.SUPER_TENANT_USER},
-            new Object[]{TestUserMode.TENANT_USER},
-            };
+                new Object[]{TestUserMode.SUPER_TENANT_USER},
+                new Object[]{TestUserMode.TENANT_USER},
+        };
     }
 
 
