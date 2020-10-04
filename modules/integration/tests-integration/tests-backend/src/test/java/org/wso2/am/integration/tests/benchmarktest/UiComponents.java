c0cb56d7a72397ef0da194c721744a30fd376991
TheRasanjana <snironr@gmail.com>
2020-10-04 23:23:43 +0530

    Update UiComponents.java
    Refactor and enable headless

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/benchmarktest/UiComponents.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/benchmarktest/UiComponents.java
@@ -1,22 +1,32 @@
+/*
+ *Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *WSO2 Inc. licenses this file to you under the Apache License,
+ *Version 2.0 (the "License"); you may not use this file except
+ *in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on an
+ *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *KIND, either express or implied.  See the License for the
+ *specific language governing permissions and limitations
+ *under the License.
+ */
+
 package org.wso2.am.integration.tests.benchmarktest;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
-import org.openqa.selenium.phantomjs.PhantomJSDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.io.File;
 import java.io.IOException;
 
 import java.util.concurrent.TimeUnit;
@@ -41,7 +51,7 @@ public class UiComponents {
         options.addArguments("--disable-gpu");
         options.addArguments(" --ignore-ssl-errors=yes");
         options.addArguments("--ignore-certificate-errors");
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
