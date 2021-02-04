df19508495999bb952b7a1f6d9573599163960f0
Mushthaq Rumy <mushthaq33@gmail.com>
2021-02-04 14:48:28 +0530

    Updating carbon-apimgt

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/header/CORSHeadersTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/header/CORSHeadersTestCase.java
@@ -227,9 +227,6 @@ public class CORSHeadersTestCase extends APIManagerLifecycleBaseTest {
     public static Object[][] userModeDataProvider() {
         return new Object[][] { new Object[] { TestUserMode.SUPER_TENANT_ADMIN },
                 new Object[] { TestUserMode.TENANT_ADMIN },
-                new Object[] { TestUserMode.SUPER_TENANT_USER_STORE_USER },
-                new Object[] { TestUserMode.SUPER_TENANT_EMAIL_USER },
-                new Object[] { TestUserMode.TENANT_EMAIL_USER },
         };
     }
 
