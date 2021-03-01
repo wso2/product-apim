5f3e33b012123985c377a85afd1608f1d0d424da
thisal <thisaltennakoon@gmail.com>
2021-03-01 17:53:58 +0530

    Enable all the test users

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/TagsRatingCommentTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/TagsRatingCommentTestCase.java
@@ -61,10 +61,10 @@ import static org.testng.Assert.assertNotEquals;
     @DataProvider
     public static Object[][] userModeDataProvider() {
         return new Object[][] { new Object[] { TestUserMode.SUPER_TENANT_ADMIN },
-//                new Object[] { TestUserMode.TENANT_ADMIN },
-//                new Object[] { TestUserMode.SUPER_TENANT_USER_STORE_USER },
-//                new Object[] { TestUserMode.SUPER_TENANT_EMAIL_USER },
-//                new Object[] { TestUserMode.TENANT_EMAIL_USER },
+                new Object[] { TestUserMode.TENANT_ADMIN },
+                new Object[] { TestUserMode.SUPER_TENANT_USER_STORE_USER },
+                new Object[] { TestUserMode.SUPER_TENANT_EMAIL_USER },
+                new Object[] { TestUserMode.TENANT_EMAIL_USER },
         };
     }
 
