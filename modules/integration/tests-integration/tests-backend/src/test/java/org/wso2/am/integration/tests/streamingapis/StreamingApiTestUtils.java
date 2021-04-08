276c5d9aaab2eff71b9b0f40ae5abc80b51c254c
chanikag <chanika@wso2.com>
2021-04-08 17:59:14 +0530

    Add testcases for webhooks usecases
    This is to verify usecases with secret

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/streamingapis/StreamingApiTestUtils.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/streamingapis/StreamingApiTestUtils.java
@@ -18,13 +18,20 @@
 
 package org.wso2.am.integration.tests.streamingapis;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
 
 /**
  * Contains utility methods for Streaming API test cases.
  */
 public class StreamingApiTestUtils {
+
+    private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
 
     /**
      * Returns the available port, within the range of given lower and upper ports, in the given host.
@@ -61,5 +68,21 @@ public class StreamingApiTestUtils {
                 }
             }
         }
+    }
+
+    public static String calculateRFC2104HMAC(String data, String key) throws NoSuchAlgorithmException,
+            InvalidKeyException {
+        SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), HMAC_SHA1_ALGORITHM);
+        Mac mac = Mac.getInstance(HMAC_SHA1_ALGORITHM);
+        mac.init(signingKey);
+        return toHexString(mac.doFinal(data.getBytes()));
+    }
+
+    private static String toHexString(byte[] bytes) {
+        Formatter formatter = new Formatter();
+        for (byte b : bytes) {
+            formatter.format("%02x", b);
+        }
+        return formatter.toString();
     }
 }
