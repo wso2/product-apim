e1a00ed5f0b36c9866f35dac5a0e2890def48c38
Tharindu Dharmarathna <tharindua@wso2.com>
2021-04-16 11:14:01 +0530

    change wso2server.sh to api-manager.sh

--- modules/distribution/product/src/main/startup-scripts/api-manager.sh
+++ modules/distribution/product/src/main/startup-scripts/api-manager.sh
@@ -203,7 +203,7 @@ elif [ "$CMD" = "start" ]; then
   fi
   export CARBON_HOME="$CARBON_HOME"
 # using nohup sh to avoid erros in solaris OS.TODO
-  nohup sh "$CARBON_HOME"/bin/wso2server.sh $args > /dev/null 2>&1 &
+  nohup sh "$CARBON_HOME"/bin/api-manager.sh $args > /dev/null 2>&1 &
   exit 0
 elif [ "$CMD" = "stop" ]; then
   export CARBON_HOME="$CARBON_HOME"
@@ -222,7 +222,7 @@ elif [ "$CMD" = "restart" ]; then
   done
 
 # using nohup sh to avoid erros in solaris OS.TODO
-  nohup sh "$CARBON_HOME"/bin/wso2server.sh $args > /dev/null 2>&1 &
+  nohup sh "$CARBON_HOME"/bin/api-manager.sh $args > /dev/null 2>&1 &
   exit 0
 elif [ "$CMD" = "test" ]; then
     JAVACMD="exec "$JAVACMD""
