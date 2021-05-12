56fbd0ae27db3d48414759881293719383d0ed3c
rashm1n <rashmin.ravindu@gmail.com>
2021-05-12 16:26:30 +0530

    Changes in API Deny Policy and API System Scopes Test Cases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APIDenyPolicyTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/APIDenyPolicyTestCase.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package org.wso2.am.integration.tests.other;
 
 import org.apache.commons.logging.Log;
@@ -14,13 +32,10 @@ import org.wso2.am.integration.clients.admin.ApiResponse;
 import org.wso2.am.integration.clients.admin.api.dto.BlockingConditionDTO;
 import org.wso2.am.integration.clients.admin.api.dto.BlockingConditionStatusDTO;
 import org.wso2.am.integration.clients.store.api.v1.dto.ApplicationDTO;
-import org.wso2.am.integration.clients.store.api.v1.dto.ApplicationKeyDTO;
-import org.wso2.am.integration.clients.store.api.v1.dto.ApplicationKeyGenerateRequestDTO;
 import org.wso2.am.integration.test.utils.base.APIMIntegrationConstants;
 import org.wso2.am.integration.test.utils.bean.APIRequest;
 import org.wso2.am.integration.tests.api.lifecycle.APIManagerLifecycleBaseTest;
 import org.wso2.carbon.automation.engine.context.TestUserMode;
-import org.wso2.carbon.automation.test.utils.http.client.HttpRequestUtil;
 import org.wso2.carbon.automation.test.utils.http.client.HttpResponse;
 
 import java.net.URL;
@@ -28,7 +43,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 public class APIDenyPolicyTestCase extends APIManagerLifecycleBaseTest {
     private final Log log = LogFactory.getLog(APIDenyPolicyTestCase.class);
