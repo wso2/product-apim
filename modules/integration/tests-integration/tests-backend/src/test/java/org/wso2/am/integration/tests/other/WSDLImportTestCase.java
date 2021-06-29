d86e84d2b3bf5f520c26f2ce6601a4112ab3b56e
Chamindu36 <cudakara@gmail.com>
2021-06-29 16:02:21 +0530

    Add WSDLImportTestCase to testing.xml file

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/WSDLImportTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/WSDLImportTestCase.java
@@ -52,8 +52,6 @@ import static org.testng.Assert.assertEquals;
 
     private String API_NAME = "WSDLImportAPI";
     private String API_CONTEXT = "wsdlimport";
-    private String WSDL_ZIP_IMPORT_API_NAME = "WSDLZipImportAPI";
-    private String WSDL_ZIP_IMPORT_API_CONTEXT = "wsdlzipimport";
     private String API_VERSION = "1.0.0";
     private String backendEndWSDL;
     private String backendEndUrl;
@@ -65,7 +63,6 @@ import static org.testng.Assert.assertEquals;
     private String userName;
     private String password;
     private String tenantDomain;
-    private String resourcePath;
     private final String KEYSTORE_FILE_PATH_CLIENT =
             TestConfigurationProvider.getResourceLocation() + File.separator + "keystores" + File.separator + "products"
                     + File.separator + "wso2carbon.jks";
@@ -133,31 +130,15 @@ import static org.testng.Assert.assertEquals;
         HttpResponse createdApiResponse = restAPIPublisher.getAPI(importApiId);
         assertEquals(Response.Status.OK.getStatusCode(), createdApiResponse.getResponseCode());
 
-        //create API by importing the WSDL definition as .zip file
-        String wsdlZipPath = FrameworkPathUtil.getSystemResourceLocation()  + "wsdl" + File.separator + "Sample.zip";
-        File file2 = new File(wsdlZipPath);
-        APIDTO apidtoZip = restAPIPublisher
-                .importWSDLSchemaDefinition(file2, backendEndUrl, additionalPropertiesObj.toString(), "SOAP");
-
-        //Make sure API is created properly
-        assertEquals(apidtoZip.getName(), WSDL_ZIP_IMPORT_API_NAME);
-        assertEquals(apidtoZip.getContext(), "/" + WSDL_ZIP_IMPORT_API_CONTEXT);
-        importZipApiId = apidtoZip.getId();
-        HttpResponse createdZipApiResponse = restAPIPublisher.getAPI(importZipApiId);
-        assertEquals(Response.Status.OK.getStatusCode(), createdZipApiResponse.getResponseCode());
-
     }
 
     @Test(groups = {"wso2.am" }, description = "Get WSDL API definition of the created API", dependsOnMethods = "testWsdlDefinitionImport")
     public void testGetWsdlDefinitions()throws Exception {
         log.info("testGetWsdlDefinition  Initiated");
+
         // get wsdl definition of the API created with .wsdl file
         ApiResponse<Void> response = restAPIPublisher.getWSDLSchemaDefinitionOfAPI(importApiId);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
-
-        // get wsdl definition of the API created with .zip file
-        ApiResponse<Void> response2 = restAPIPublisher.getWSDLSchemaDefinitionOfAPI(importZipApiId);
-        assertEquals(Response.Status.OK.getStatusCode(), response2.getStatusCode());
     }
 
     @Test(groups = {"wso2.am" }, description = "Download WSDL API definition of the created API from the store", dependsOnMethods = "testGetWsdlDefinitions")
@@ -169,16 +150,10 @@ import static org.testng.Assert.assertEquals;
         org.wso2.am.integration.clients.store.api.ApiResponse<Void> response = restAPIStore
                 .downloadWSDLSchemaDefinitionOfAPI(importApiId, environmentName);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
-
-        //wsdl definition of the API created with .wsdl file from the store
-        org.wso2.am.integration.clients.store.api.ApiResponse<Void> response2 = restAPIStore
-                .downloadWSDLSchemaDefinitionOfAPI(importZipApiId, environmentName);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
     }
 
     @AfterClass(alwaysRun = true) void destroy() throws Exception {
         restAPIPublisher.deleteAPI(importApiId);
-        restAPIPublisher.deleteAPI(importZipApiId);
     }
 
 }
