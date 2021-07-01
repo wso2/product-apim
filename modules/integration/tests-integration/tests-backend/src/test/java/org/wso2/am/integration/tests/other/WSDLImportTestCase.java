f0464d16833c32451e1aaa0b179c77864b43b2ca
Chamindu36 <cudakara@gmail.com>
2021-07-01 14:13:18 +0530

    Add archive zip import related testcases

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/WSDLImportTestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/other/WSDLImportTestCase.java
@@ -52,6 +52,8 @@ import static org.testng.Assert.assertEquals;
 
     private String API_NAME = "WSDLImportAPI";
     private String API_CONTEXT = "wsdlimport";
+    private String WSDL_ZIP_API_NAME = "WSDLImportAPIWithZip";
+    private String WSDL_ZIP_API_CONTEXT = "wsdlimportwithzip";
     private String API_VERSION = "1.0.0";
     private String backendEndWSDL;
     private String backendEndUrl;
@@ -130,6 +132,24 @@ import static org.testng.Assert.assertEquals;
         HttpResponse createdApiResponse = restAPIPublisher.getAPI(importApiId);
         assertEquals(Response.Status.OK.getStatusCode(), createdApiResponse.getResponseCode());
 
+        //create API by importing the WSDL definition as .zip file (using archive)
+        additionalPropertiesObj.put("provider", user.getUserName());
+        additionalPropertiesObj.put("name", WSDL_ZIP_API_NAME);
+        additionalPropertiesObj.put("version", API_VERSION);
+        additionalPropertiesObj.put("context", WSDL_ZIP_API_CONTEXT);
+        String wsdlZipDefinitionPath = FrameworkPathUtil.getSystemResourceLocation()  + "wsdl" + File.separator + "Sample.zip";
+
+        File zipFile = new File(wsdlZipDefinitionPath);
+        APIDTO zipApidto = restAPIPublisher
+                .importWSDLSchemaDefinition(zipFile, backendEndUrl, additionalPropertiesObj.toString(), "SOAP");
+
+        //Make sure API is created properly
+        assertEquals(zipApidto.getName(), WSDL_ZIP_API_NAME);
+        assertEquals(zipApidto.getContext(), "/" + WSDL_ZIP_API_CONTEXT);
+        importZipApiId = zipApidto.getId();
+        HttpResponse createdApiResponse2 = restAPIPublisher.getAPI(importZipApiId);
+        assertEquals(Response.Status.OK.getStatusCode(), createdApiResponse2.getResponseCode());
+
     }
 
     @Test(groups = {"wso2.am" }, description = "Get WSDL API definition of the created API", dependsOnMethods = "testWsdlDefinitionImport")
@@ -139,6 +159,10 @@ import static org.testng.Assert.assertEquals;
         // get wsdl definition of the API created with .wsdl file
         ApiResponse<Void> response = restAPIPublisher.getWSDLSchemaDefinitionOfAPI(importApiId);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
+
+        // get wsdl definition of the API created with .zip file
+        ApiResponse<Void> zipFlowResponse = restAPIPublisher.getWSDLSchemaDefinitionOfAPI(importZipApiId);
+        assertEquals(Response.Status.OK.getStatusCode(), zipFlowResponse.getStatusCode());
     }
 
     @Test(groups = {"wso2.am" }, description = "Download WSDL API definition of the created API from the store", dependsOnMethods = "testGetWsdlDefinitions")
@@ -150,10 +174,16 @@ import static org.testng.Assert.assertEquals;
         org.wso2.am.integration.clients.store.api.ApiResponse<Void> response = restAPIStore
                 .downloadWSDLSchemaDefinitionOfAPI(importApiId, environmentName);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatusCode());
+
+        //wsdl definition of the API created with .zip file from the store
+        org.wso2.am.integration.clients.store.api.ApiResponse<Void> zipFlowResponse = restAPIStore
+                .downloadWSDLSchemaDefinitionOfAPI(importZipApiId, environmentName);
+        assertEquals(Response.Status.OK.getStatusCode(), zipFlowResponse.getStatusCode());
     }
 
     @AfterClass(alwaysRun = true) void destroy() throws Exception {
         restAPIPublisher.deleteAPI(importApiId);
+        restAPIPublisher.deleteAPI(importZipApiId);
     }
 
 }
