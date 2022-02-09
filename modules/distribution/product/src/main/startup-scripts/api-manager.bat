e8576a6ecd1e9b0076a7c85c7cf0aa4ed2494fb6
tharindud <tharindua@wso2.com>
2022-02-09 12:35:11 +0530

    enable asyync logging

--- modules/distribution/product/src/main/startup-scripts/api-manager.bat
+++ modules/distribution/product/src/main/startup-scripts/api-manager.bat
@@ -217,6 +217,7 @@ set CMD_LINE_ARGS=%CMD_LINE_ARGS% -Dcom.atomikos.icatch.hide_init_file_path="tru
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% -Dorg.apache.jasper.runtime.BodyContentImpl.LIMIT_BUFFER=true -Dcom.sun.jndi.ldap.connect.pool.authentication=simple -Dcom.sun.jndi.ldap.connect.pool.timeout=3000
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% -Dorg.terracotta.quartz.skipUpdateCheck=true -Dcarbon.classpath=%CARBON_CLASSPATH% -Dfile.encoding=UTF8 -DworkerNode=false -Dcarbon.new.config.dir.path="%CARBON_HOME%\repository\resources\conf"
 set CMD_LINE_ARGS=%CMD_LINE_ARGS% -Djava.endorsed.dirs=%JAVA_ENDORSED% -Dorg.opensaml.httpclient.https.disableHostnameVerification=true -Dhttpclient.hostnameVerifier="DefaultAndLocalhost"
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% -Dlog4j2.contextSelector="org.apache.logging.log4j.core.async.AsyncLoggerContextSelector"
 
 :runJava
 echo JAVA_HOME environment variable is set to %JAVA_HOME%
--- modules/distribution/product/src/main/startup-scripts/api-manager.sh
+++ modules/distribution/product/src/main/startup-scripts/api-manager.sh
@@ -353,6 +353,7 @@ do
     -DenableCorrelationLogs=false \
     -Dcarbon.new.config.dir.path="$CARBON_HOME/repository/resources/conf" \
     -Djavax.xml.xpath.XPathFactory:http://java.sun.com/jaxp/xpath/dom=net.sf.saxon.xpath.XPathFactoryImpl \
+    -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
     org.wso2.carbon.bootstrap.Bootstrap $*
     status=$?
 done
