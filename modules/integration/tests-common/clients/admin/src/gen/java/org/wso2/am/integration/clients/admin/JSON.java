1375416afca43102c195da457215e5ddf8bf79c6
RansikaG <ransika1998@gmail.com>
2022-06-17 07:47:28 +0530

    adding generated files

--- modules/integration/tests-common/clients/admin/src/gen/java/org/wso2/am/integration/clients/admin/JSON.java
+++ modules/integration/tests-common/clients/admin/src/gen/java/org/wso2/am/integration/clients/admin/JSON.java
@@ -61,6 +61,15 @@ public class JSON {
                                 getDiscriminatorValue(readElement, "type"));
                     }
           })
+                .registerTypeSelector(ThrottlePolicyDetailsDTO.class, new TypeSelector() {
+                    @Override
+                    public Class getClassForElement(JsonElement readElement) {
+                        Map<String, Class> classByDiscriminatorValue = new HashMap<String, Class>();
+                        classByDiscriminatorValue.put("ThrottlePolicyDetails", ThrottlePolicyDetailsDTO.class);
+                        return getClassByDiscriminator(classByDiscriminatorValue,
+                                getDiscriminatorValue(readElement, "type"));
+                    }
+          })
         ;
         GsonBuilder builder = fireBuilder.createGsonBuilder();
         return builder;
