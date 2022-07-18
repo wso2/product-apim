52fc1930b82824659159ebc4ddd852470f8ca6d4
sachini <dmsachiniacc@gmail.com>
2022-07-18 20:32:50 +0530

    Comment unused user roles

--- modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/restapi/ServiceCatalogRestAPITestCase.java
+++ modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/restapi/ServiceCatalogRestAPITestCase.java
@@ -42,10 +42,10 @@ public class ServiceCatalogRestAPITestCase extends APIMIntegrationBaseTest {
     public static Object[][] userModeDataProvider() {
         return new Object[][]{
                 new Object[]{TestUserMode.SUPER_TENANT_ADMIN},
-                new Object[]{TestUserMode.TENANT_ADMIN},
-                new Object[]{TestUserMode.SUPER_TENANT_USER_STORE_USER},
-                new Object[]{TestUserMode.SUPER_TENANT_EMAIL_USER},
-                new Object[]{TestUserMode.TENANT_EMAIL_USER},
+//                new Object[]{TestUserMode.TENANT_ADMIN},
+//                new Object[]{TestUserMode.SUPER_TENANT_USER_STORE_USER},
+//                new Object[]{TestUserMode.SUPER_TENANT_EMAIL_USER},
+//                new Object[]{TestUserMode.TENANT_EMAIL_USER},
         };
     }
 
