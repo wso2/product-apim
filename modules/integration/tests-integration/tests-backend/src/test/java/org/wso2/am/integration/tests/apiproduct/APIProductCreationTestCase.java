shilmyhasan/product-apim-1

Modify integration tests adding the newly introduced 'policyType' field in OperationPolicyDTO

modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/apiproduct/APIProductCreationTestCase.java
@@ -84,6 +84,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
     private static final String RESTRICTED_SUBSCRIBER = "restricted_user";
     private static final String STANDARD_SUBSCRIBER = "standard_user";
     private static final String PASSWORD = "$3213#@sd";
+    private static final String POLICY_TYPE_API = "api";
     private static final String RESTRICTED_ROLE = "restricted_role";
     private static final String SCOPE = "restricted_scope";
     private ApiTestHelper apiTestHelper;
@@ -470,8 +471,8 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         List<APIDTO> apisToBeUsed = new ArrayList<>();
         APIDTO api = apiTestHelper.createAnApi(getBackendEndServiceEndPointHttp("wildcard/resources"));
         APIOperationPoliciesDTO apiOperationPoliciesDTO = new APIOperationPoliciesDTO();
-        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", null));
-        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", POLICY_TYPE_API, null));
+        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", POLICY_TYPE_API, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -539,7 +540,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
                 expectedResponse, headers);
         // Step 9 : Change In mediation Sequence in base API and verify change reflect in APIProduct.
         Assert.assertNotNull(api);
-        apiOperationPoliciesDTO.setRequest(getPolicyList("xmlToJson", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("xmlToJson", POLICY_TYPE_API, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -563,8 +564,8 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         List<APIDTO> apisToBeUsed = new ArrayList<>();
         APIDTO api = apiTestHelper.createAnApi(getBackendEndServiceEndPointHttp("wildcard/resources"));
         APIOperationPoliciesDTO apiOperationPoliciesDTO = new APIOperationPoliciesDTO();
-        apiOperationPoliciesDTO.setResponse(getPolicyList("xmlToJson", null));
-        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", null));
+        apiOperationPoliciesDTO.setResponse(getPolicyList("xmlToJson", POLICY_TYPE_API, null));
+        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", POLICY_TYPE_API, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -634,7 +635,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
                 jsonResponseBody, headers);
         // Step 9 : Change In mediation Sequence in base API and verify change reflect in APIProduct.
         Assert.assertNotNull(api);
-        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", POLICY_TYPE_API, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -851,11 +852,13 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         return restAPIPublisher.getApiProduct(uuid);
     }
 
-    public List<OperationPolicyDTO> getPolicyList(String policyName, Map<String, Object> attributeMap) {
+    public List<OperationPolicyDTO> getPolicyList(String policyName, String policyType, Map<String,
+            Object> attributeMap) {
 
         List<OperationPolicyDTO> policyList = new ArrayList<>();
         OperationPolicyDTO policyDTO = new OperationPolicyDTO();
         policyDTO.setPolicyName(policyName);
+        policyDTO.setPolicyType(policyType);
         policyDTO.setParameters(attributeMap);
         policyList.add(policyDTO);
 

wso2/product-apim

modules/integration/tests-integration/tests-backend/src/test/java/org/wso2/am/integration/tests/apiproduct/APIProductCreationTestCase.java
@@ -84,6 +84,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
     private static final String RESTRICTED_SUBSCRIBER = "restricted_user";
     private static final String STANDARD_SUBSCRIBER = "standard_user";
     private static final String PASSWORD = "$3213#@sd";
+    private static final String POLICY_TYPE_COMMON = "common";
     private static final String RESTRICTED_ROLE = "restricted_role";
     private static final String SCOPE = "restricted_scope";
     private ApiTestHelper apiTestHelper;
@@ -470,8 +471,8 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         List<APIDTO> apisToBeUsed = new ArrayList<>();
         APIDTO api = apiTestHelper.createAnApi(getBackendEndServiceEndPointHttp("wildcard/resources"));
         APIOperationPoliciesDTO apiOperationPoliciesDTO = new APIOperationPoliciesDTO();
-        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", null));
-        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", POLICY_TYPE_COMMON, null));
+        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", POLICY_TYPE_COMMON, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -539,7 +540,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
                 expectedResponse, headers);
         // Step 9 : Change In mediation Sequence in base API and verify change reflect in APIProduct.
         Assert.assertNotNull(api);
-        apiOperationPoliciesDTO.setRequest(getPolicyList("xmlToJson", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("xmlToJson", POLICY_TYPE_COMMON, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -563,8 +564,8 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         List<APIDTO> apisToBeUsed = new ArrayList<>();
         APIDTO api = apiTestHelper.createAnApi(getBackendEndServiceEndPointHttp("wildcard/resources"));
         APIOperationPoliciesDTO apiOperationPoliciesDTO = new APIOperationPoliciesDTO();
-        apiOperationPoliciesDTO.setResponse(getPolicyList("xmlToJson", null));
-        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", null));
+        apiOperationPoliciesDTO.setResponse(getPolicyList("xmlToJson", POLICY_TYPE_COMMON, null));
+        apiOperationPoliciesDTO.setFault(getPolicyList("jsonFault", POLICY_TYPE_COMMON, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -634,7 +635,7 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
                 jsonResponseBody, headers);
         // Step 9 : Change In mediation Sequence in base API and verify change reflect in APIProduct.
         Assert.assertNotNull(api);
-        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", null));
+        apiOperationPoliciesDTO.setRequest(getPolicyList("jsonToXML", POLICY_TYPE_COMMON, null));
         for (APIOperationsDTO operationsDTO : api.getOperations()) {
             operationsDTO.setOperationPolicies(apiOperationPoliciesDTO);
         }
@@ -851,11 +852,13 @@ public class APIProductCreationTestCase extends APIManagerLifecycleBaseTest {
         return restAPIPublisher.getApiProduct(uuid);
     }
 
-    public List<OperationPolicyDTO> getPolicyList(String policyName, Map<String, Object> attributeMap) {
+    public List<OperationPolicyDTO> getPolicyList(String policyName, String policyType, Map<String,
+            Object> attributeMap) {
 
         List<OperationPolicyDTO> policyList = new ArrayList<>();
         OperationPolicyDTO policyDTO = new OperationPolicyDTO();
         policyDTO.setPolicyName(policyName);
+        policyDTO.setPolicyType(policyType);
         policyDTO.setParameters(attributeMap);
         policyList.add(policyDTO);
 