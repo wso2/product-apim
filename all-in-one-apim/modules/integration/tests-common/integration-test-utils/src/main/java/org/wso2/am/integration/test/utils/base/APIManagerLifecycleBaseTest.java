b4df66b5c128b385232ee1d9332d269b60a7a694
AnuGayan <anushaj@wso2.com>
2025-10-15 09:00:40 +0530

    Move APIManagerLifecycleBaseTest to common utils

--- all-in-one-apim/modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/utils/base/APIManagerLifecycleBaseTest.java
+++ all-in-one-apim/modules/integration/tests-common/integration-test-utils/src/main/java/org/wso2/am/integration/test/utils/base/APIManagerLifecycleBaseTest.java
@@ -1,8 +1,8 @@
 /*
  *
- * Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
