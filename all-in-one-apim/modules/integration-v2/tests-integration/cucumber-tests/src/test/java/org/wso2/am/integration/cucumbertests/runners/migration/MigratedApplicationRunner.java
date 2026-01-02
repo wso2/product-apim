fec404ff72cda491c5da773ab1f9c46e9c6a3778
Milanka00 <milankatharangana01@gmail.com>
2026-01-02 07:49:05 +0530

    Updating Runners with necessary users

--- all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/migration/MigratedApplicationRunner.java
+++ all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/migration/MigratedApplicationRunner.java
@@ -55,7 +55,7 @@ public class MigratedApplicationRunner extends AbstractTestNGCucumberTests {
     public Object[][] userModeDataProvider() {
         return new Object[][]{
                 {"carbon.super", "admin"}, // Super tenant admin
-//                {"adpsample.com", "admin"},
+                {"adpsample.com", "admin"},
         };
     }
 }
