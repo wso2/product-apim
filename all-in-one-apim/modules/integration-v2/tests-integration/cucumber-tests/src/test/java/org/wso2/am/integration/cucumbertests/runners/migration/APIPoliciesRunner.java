509068a3f1628d9da82ffaecb60e3009291e50ab
Milanka00 <milankatharangana01@gmail.com>
2026-01-06 11:33:57 +0530

    Improvements for suggested review changes

--- all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/migration/APIPoliciesRunner.java
+++ all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/runners/migration/APIPoliciesRunner.java
@@ -1,8 +1,29 @@
+/*
+ *  Copyright (c) 2026, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.wso2.am.integration.cucumbertests.runners.migration;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
 import org.wso2.am.integration.cucumbertests.utils.TestContext;
 import org.wso2.am.integration.cucumbertests.utils.Utils;
 import org.wso2.am.integration.test.utils.Constants;
@@ -13,7 +34,7 @@ import org.wso2.carbon.automation.engine.context.beans.User;
 @CucumberOptions(
         features = "src/test/resources/features/migration/api_policies.feature",
         glue = "org.wso2.am.integration.cucumbertests.stepdefinitions",
-        plugin = {"pretty", "html:target/cucumber-report/createAPI.html"}
+        plugin = {"pretty", "html:target/cucumber-report/api-policies.html"}
 )
 
 @Test(groups = {"migrationTest"})
@@ -37,7 +58,7 @@ public class APIPoliciesRunner extends AbstractTestNGCucumberTests {
                 ? tenant.getTenantAdmin()
                 : tenant.getTenantUser(testUserKey);
         tenant.setContextUser(user);
-        TestContext.set("currentTenant", tenant);
+        TestContext.set(Constants.CURRENT_TENANT, tenant);
 
         // Restart NodeApp server to ensure clean backend state for each tenant
         NodeAppServer.getInstance().restart();
@@ -45,7 +66,7 @@ public class APIPoliciesRunner extends AbstractTestNGCucumberTests {
 
     @AfterClass(alwaysRun = true)
     public void afterClass() {
-        TestContext.remove("currentTenant");
+        TestContext.remove(Constants.CURRENT_TENANT);
     }
 
     @Factory(dataProvider = "userModeDataProvider")
@@ -60,8 +81,8 @@ public class APIPoliciesRunner extends AbstractTestNGCucumberTests {
     public Object[][] userModeDataProvider() {
         return new Object[][]{
                 // non admin users are not authorized to create policies
-                {"carbon.super", "admin"}, // Super tenant admin
-                {"adpsample.com", "admin"}, // Tenant admin
+                {Constants.SUPER_TENANT_DOMAIN, Constants.ADMIN_USER_KEY}, // Super tenant admin
+                {Constants.ADPSAMPLE_TENANT_DOMAIN, Constants.ADPSAMPLE_ADMIN_USER_KEY}, // Tenant admin
         };
     }
 }
