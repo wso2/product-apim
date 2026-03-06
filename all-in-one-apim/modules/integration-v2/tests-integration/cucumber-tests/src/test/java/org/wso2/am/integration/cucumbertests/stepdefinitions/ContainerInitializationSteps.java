64f630f17bea4efb7af6dd69284ccbd666deb9e0
venukshiMendis <venukshinaradha@gmail.com>
2026-03-06 12:29:36 +0530

    Added migrated custom policy verification

--- all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/stepdefinitions/ContainerInitializationSteps.java
+++ all-in-one-apim/modules/integration-v2/tests-integration/cucumber-tests/src/test/java/org/wso2/am/integration/cucumbertests/stepdefinitions/ContainerInitializationSteps.java
@@ -67,10 +67,12 @@ public class ContainerInitializationSteps {
             tomlContent = Utils.mergeToml(baseTomlPath, tomlChangesPath);
         }
 
-        // Append encryption config to the deployment.toml in the expected format
-        String encryptionTomlPath = Paths.get(callerModuleDir, Constants.ENCRYPTION_TOML_PATH).toString();
-        String encryptionTomlContent = Files.readString(Path.of(encryptionTomlPath));
-        tomlContent = tomlContent + System.lineSeparator() + System.lineSeparator() + encryptionTomlContent;
+        if (Constants.MIGRATION_PROFILE.equals(activeProfile)) {
+            // Append encryption config to the deployment.toml in the expected format
+            String encryptionTomlPath = Paths.get(callerModuleDir, Constants.ENCRYPTION_TOML_PATH).toString();
+            String encryptionTomlContent = Files.readString(Path.of(encryptionTomlPath));
+            tomlContent = tomlContent + System.lineSeparator() + System.lineSeparator() + encryptionTomlContent;
+        }
 
         APIMContainer apimContainer = new APIMContainer(label, tomlContent);
         apimContainer.start();
